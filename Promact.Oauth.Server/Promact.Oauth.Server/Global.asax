8c90d7ecc547f4a47db137ad5fb286e8840a6963
roshni@promactinfo.com <Promact2015>
2016-07-20 15:40:29 +0530

    Updated project to aspnetcore

--- Promact.Oauth.Server/Promact.Oauth.Server/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Promact.Oauth.Server.MvcApplication" Language="C#" %>
---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Oauth.Server.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
