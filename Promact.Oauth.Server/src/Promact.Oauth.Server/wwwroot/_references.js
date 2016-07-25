d368226b377495fca76357d6721329aaff02dce0
Ronak <ronak@PR2C4R4Y2015>
2016-07-25 14:44:39 +0530

    checkout issue

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/_references.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/_references.js
@@ -1,7 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
-/// <reference path="app/app.component.js" />
-/// <reference path="app/main.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
