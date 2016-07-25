c05ca8f6c3b1c75a6637d1251f8af3cecdec6655
rajdeep <rajdeep@PR2C4R1Y2015>
2016-07-25 15:20:45 +0530

    added user model

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Models/ApplicationUser.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Models/ApplicationUser.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Promact.Oauth.Server.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
     }
 }
