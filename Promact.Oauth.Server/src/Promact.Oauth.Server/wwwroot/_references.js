f936969522db7d6420342f66f0fe5e3b87f348e3
Ronak <ronak.shah@promactinfo.com>
2016-07-25 18:45:29 +0530

    autosync off

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/_references.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/_references.js
@@ -1,5 +1,4 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile.js" />
+﻿/// <reference path="../gulpfile.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
