cb87fb2501a1b64077915fff2799f258c506a419
rajdeep <rajdeep@PR2C4R1Y2015>
2016-07-25 15:33:12 +0530

    Adding Seed Method

---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Seed/EnsureSeedData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Oauth.Server.Seed
+{
+    public class EnsureSeedData
+    {
+        //Seeds the database wuth the initial user Admin
+        public static void Seed()
+        {
+
+        }
+    }
+}
