65a98b97a1e8ca6e3c164769c52c7b756d587abf
rushi <rushi_216@yahoo.com>
2016-07-26 11:41:01 +0530

    removed unwanted whitespaces

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.component.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from "@angular/core";
+﻿import {Component} from "@angular/core";
 @Component({
     selector: "my-app",
     template: "<h1>My First Angular 2 App</h1>"
--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/main.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/main.ts
@@ -1,5 +1,5 @@
-﻿import { bootstrap }    from "@angular/platform-browser-dynamic";
+﻿import {bootstrap}    from "@angular/platform-browser-dynamic";
 import {enableProdMode} from '@angular/core';
-import { AppComponent } from "./app.component";
+import {AppComponent} from "./app.component";
 enableProdMode();
 bootstrap(AppComponent);
