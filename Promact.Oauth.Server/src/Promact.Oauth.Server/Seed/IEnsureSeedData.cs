b72e8197300522caddfb229705f86a02b676ce52
rajdeep <rajdeep@PR2C4R1Y2015>
2016-07-29 15:51:53 +0530

    Seed Data Chnages

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Seed/IEnsureSeedData.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Seed/IEnsureSeedData.cs
@@ -7,6 +7,6 @@ namespace Promact.Oauth.Server.Seed
 {
     public interface IEnsureSeedData
     {
-        void Seed(IServiceProvider serviceProvider);
+        void Seed(IServiceProvider serviceProvider);  
     }
 }
