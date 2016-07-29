a24d5b55c8ccb655cb5652c42edc5fa53e462d8b
rajdeep <rajdeep@PR2C4R1Y2015>
2016-07-29 17:15:27 +0530

    test

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user.routes.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user.routes.ts
@@ -4,7 +4,7 @@ import {UserComponent} from './user.component';
 import {UserListComponent} from './user-list/user-list.component';
 
 export const projectRoutes: RouterConfig = [{
-    path: "user",
+    path: "user", 
     component: UserComponent,
     children: [
         { path: '', component: UserListComponent }
