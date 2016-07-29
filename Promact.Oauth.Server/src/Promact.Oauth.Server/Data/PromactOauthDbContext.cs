362698581f5e11f47684cf98a8636a52b56371cb
rajdeep <rajdeep@PR2C4R1Y2015>
2016-07-29 17:57:02 +0530

    Modified application dbContext

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Data/PromactOauthDbContext.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Data/PromactOauthDbContext.cs
@@ -29,6 +29,7 @@ namespace Promact.Oauth.Server.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+            
         }
         
     }
