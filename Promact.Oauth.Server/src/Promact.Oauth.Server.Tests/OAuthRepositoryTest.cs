86f6567cceeac58e473cf3e80f1a6d1244198289
Siddhartha <Siddhartha@PR2C5R2Y2015>
2016-08-11 18:10:06 +0530

    Update inOAuth Unit testing

--- Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/OAuthRepositoryTest.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/OAuthRepositoryTest.cs
@@ -16,6 +16,9 @@ namespace Promact.Oauth.Server.Tests
             _oAuthRepository = serviceProvider.GetService<IOAuthRepository>();
         }
 
+        /// <summary>
+        /// Checking client is exist or not. If not it will create OAuth response for request.
+        /// </summary>
         [Fact, Trait("Category", "Required")]
         public void OAuthClientChecking()
         {
@@ -23,6 +26,9 @@ namespace Promact.Oauth.Server.Tests
             Assert.Null(response);
         }
 
+        /// <summary>
+        /// Checking method of client version.
+        /// </summary>
         [Fact, Trait("Category", "Required")]
         public async void GetAppDetailsFromClientNotNull()
         {
@@ -30,6 +36,9 @@ namespace Promact.Oauth.Server.Tests
             Assert.NotNull(response);
         }
 
+        /// <summary>
+        /// Checking method of client version.
+        /// </summary>
         [Fact, Trait("Category", "Required")]
         public async void GetAppDetailsFromClientTrueValue()
         {
@@ -37,6 +46,9 @@ namespace Promact.Oauth.Server.Tests
             Assert.Equal(response.ClientSecret, ClientSecret);
         }
 
+        /// <summary>
+        /// Static Variables to be used in OAuth Repository Test
+        /// </summary>
         private static string Email = "siddhartha@promactinfo.com";
         private static string ClientId = "dsfargazdfvfhfghkf";
         private static string RedirectUrl = "http://localhost:28182/oAuth/RefreshToken";
