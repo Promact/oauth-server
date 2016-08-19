9fec2446329f6954d8d7f500dc6c992454a44866
Ronak <ronak@PR2C4R4Y2015>
2016-08-19 12:18:02 +0530

    changes in project-add-component.spec.ts

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-add/project-add.component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-add/project-add.component.spec.ts
@@ -1,24 +1,24 @@
-﻿import {addProviders, inject} from '@angular/core/testing';
-//import {it, describe, expect} from "@angular/core/testing";
-describe('Test', () => {
-    it('Should test matchers', () => {
-        let _undefined, _defined = true;
-        expect("a" + "b").toBe("ab");
-        expect(_undefined).toBeUndefined();
-        expect(_defined).toBeDefined();
-        expect(!_defined).toBeFalsy();
-        expect(_defined).toBeTruthy();
-        expect(null).toBeNull();
-        expect(1 + 1).toEqual(2);
-        expect(5).toBeGreaterThan(4);
-        expect(5).toBeLessThan(6);
-        expect("abcdbca").toContain("bcd");
-        expect([4, 5, 6]).toContain(5);
-        expect("abcdefgh").toMatch(/efg/);
-        expect("abcdbca").not.toContain("xyz");
-        expect("abcdefgh").not.toMatch(/123/);
-    });
-});
+﻿//import {TestBed, inject} from '@angular/core/testing';
+////import {it, describe, expect} from "@angular/core/testing";
+//describe('Test', () => {
+//    it('Should test matchers', () => {
+//        let _undefined, _defined = true;
+//        expect("a" + "b").toBe("ab");
+//        expect(_undefined).toBeUndefined();
+//        expect(_defined).toBeDefined();
+//        expect(!_defined).toBeFalsy();
+//        expect(_defined).toBeTruthy();
+//        expect(null).toBeNull();
+//        expect(1 + 1).toEqual(2);
+//        expect(5).toBeGreaterThan(4);
+//        expect(5).toBeLessThan(6);
+//        expect("abcdbca").toContain("bcd");
+//        expect([4, 5, 6]).toContain(5);
+//        expect("abcdefgh").toMatch(/efg/);
+//        expect("abcdbca").not.toContain("xyz");
+//        expect("abcdefgh").not.toMatch(/123/);
+//    });
+//});
 
 //import {addProviders, inject} from '@angular/core/testing';
 //import {addProviders, async, inject, TestBed, ComponentFixture} from '@angular/core/testing';
