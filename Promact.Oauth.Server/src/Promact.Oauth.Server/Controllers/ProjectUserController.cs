391ddddb3262aaaf3ac80fd9c455ead170429bee
Siddhartha <Siddhartha@PR2C5R2Y2015>
2016-08-20 16:01:46 +0530

    Added Custom Attribute and Test for its working

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectUserController.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectUserController.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Promact.Oauth.Server.Data;
 using Promact.Oauth.Server.Repository;
+using Promact.Oauth.Server.Repository.OAuthRepository;
+using Promact.Oauth.Server.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Promact.Oauth.Server.Controllers
 {
+    [ServiceFilter(typeof(CustomAttribute))]
     [Route("api/[controller]")]
-    public class ProjectUserController:BaseController
+    public class ProjectUserController : BaseController
     {
         private readonly IUserRepository _userRepository;
         public ProjectUserController(IUserRepository userRepository)
@@ -36,9 +42,9 @@ namespace Promact.Oauth.Server.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("teamLeaderDetails/{userFirstName}")]
-        public IActionResult TeamLeaderByUserId(string userFirstName)
+        public async Task<IActionResult> TeamLeaderByUserId(string userFirstName)
         {
-            var user = _userRepository.TeamLeaderByUserId(userFirstName);
+            var user = await _userRepository.TeamLeaderByUserId(userFirstName);
             return Ok(user);
         }
 
---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Services/CustomAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Promact.Oauth.Server.Repository.OAuthRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Promact.Oauth.Server.Services
+{
+    public class CustomAttribute : ActionFilterAttribute
+    {
+        private readonly IOAuthRepository _oAuthRepository;
+        public CustomAttribute()
+        {
+
+        }
+        public CustomAttribute(IOAuthRepository oAuthRepository)
+        {
+            _oAuthRepository = oAuthRepository;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var token = filterContext.HttpContext.Request.Headers["Authorization"].ToString();
+            var accessToken = token.Split(' ');
+            var data = _oAuthRepository.GetDetailsClientByAccessToken(accessToken[1]);
+            if (data == false)
+            {
+                base.OnActionExecuting(filterContext);
+                filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                filterContext.Result = new JsonResult("Forbidden");
+            }
+        }
+    }
+}
