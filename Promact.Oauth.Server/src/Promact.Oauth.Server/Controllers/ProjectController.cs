247fe133390506d1ace758d0ddc56a78dd0acbc2
gourav <gourav@PR2C4R3Y2015>
2016-09-01 18:58:25 +0530

    added methods in user repository and  project repository to return data from requested from erp server

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectController.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectController.cs
@@ -170,5 +170,14 @@ namespace Promact.Oauth.Server.Controllers
             List<UserAc> projectUsers = _projectRepository.GetProjectUsersByTeamLeaderId(teamLeaderId);
             return projectUsers;
         }
+
+
+        [HttpGet]
+        [Route("projectUsersById/{teamLeaderId}")]
+        public List<UserAc> GetProjectUsersByTeamLeaderId(string teamLeaderId)
+        {
+            List<UserAc> projectUsers = _projectRepository.GetProjectUsersByTeamLeaderId(teamLeaderId);
+            return projectUsers;
+        }
     }
 }
