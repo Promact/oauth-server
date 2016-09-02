15e525ae1e3caa7fc004b60d0e77210d3bf025a0
Chintan Shah <chintan.shah12@outlook.com>
2016-09-03 00:24:02 +0530

    Updated KoreBuild.ps1 to try few things

--- KoreBuild.ps1
+++ KoreBuild.ps1
@@ -1,170 +1,64 @@
-#requires -version 4
-
-Write-Host -ForegroundColor Green "Starting KoreBuild 2.0 ..."
-
-$RepositoryRoot = Convert-Path (Get-Location)
-$BuildRoot = Join-Path $RepositoryRoot ".build"
-$KoreBuildRoot = (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)))
-$ArtifactsDir = Join-Path $RepositoryRoot "artifacts"
-
-$env:REPO_FOLDER = $RepositoryRoot
-$env:KOREBUILD_FOLDER = $KoreBuildRoot
-
-$RID = "win7-x64";
-if($env:PROCESSOR_ARCHITECTURE -eq "x86") {
-    $RID = "win7-x86";
-}
-
-$MSBuildDir = Join-Path $BuildRoot "MSBuildTools"
-$ToolsDir = Join-Path $BuildRoot "Tools"
-
-$KoreBuildLog = Join-Path $BuildRoot "korebuild.log"
-if(Test-Path $KoreBuildLog) {
-    del $KoreBuildLog
-}
-
-$MSBuildLog = Join-Path $BuildRoot "korebuild.msbuild.log"
-if(Test-Path $MSBuildLog) {
-    del $MSBuildLog
-}
-
-$MSBuildResponseFile = Join-Path $BuildRoot "korebuild.msbuild.rsp"
-if(Test-Path $MSBuildResponseFile) {
-    del $MSBuildResponseFile
-}
-
-
-function exec($cmd) {
-    $cmdName = [IO.Path]::GetFileName($cmd)
-    Write-Host -ForegroundColor DarkGray "> $cmdName $args"
-    "`r`n>>>>> $cmd $args <<<<<`r`n" >> $KoreBuildLog
-    & $cmd @args 2>&1 >> $KoreBuildLog
-    $exitCode = $LASTEXITCODE
-    if($exitCode -ne 0) {
-        throw "'$cmdName $args' failed with exit code: $exitCode. See '$ArtifactsDir\korebuild.log' for details"
-    }
-}
-
-function EnsureDotNet() {
-    $dotnetVersionFile = "$KoreBuildRoot\build\cli.version.win"
-    $dotnetChannel = "rel-1.0.0"
-    $dotnetVersion = Get-Content $dotnetVersionFile
-
-    if ($env:KOREBUILD_DOTNET_CHANNEL)
-    {
-        $dotnetChannel = $env:KOREBUILD_DOTNET_CHANNEL
-    }
-    if ($env:KOREBUILD_DOTNET_VERSION)
-    {
-        $dotnetVersion = $env:KOREBUILD_DOTNET_VERSION
-    }
-
-    $dotnetLocalInstallFolder = "$env:LOCALAPPDATA\Microsoft\dotnet\"
-    $newPath = "$dotnetLocalInstallFolder;$env:PATH"
-    if ($env:KOREBUILD_SKIP_RUNTIME_INSTALL -eq "1")
-    {
-        Write-Host -ForegroundColor Green "Skipping runtime installation because KOREBUILD_SKIP_RUNTIME_INSTALL = 1"
-        # Add to the _end_ of the path in case preferred .NET CLI is not in the default location.
-        $newPath = "$env:PATH;$dotnetLocalInstallFolder"
-    }
-    else
-    {
-        Write-Host -ForegroundColor Green "Installing .NET Command-Line Tools ..."
-        exec "$KoreBuildRoot\build\dotnet\dotnet-install.ps1" -Channel $dotnetChannel -Version $dotnetVersion -Architecture x64
-    }
-    if (!($env:Path.Split(';') -icontains $dotnetLocalInstallFolder))
-    {
-        Write-Host -ForegroundColor Green "Adding $dotnetLocalInstallFolder to PATH"
-        $env:Path = "$newPath"
-    }
-
-    # workaround for CLI issue: https://github.com/dotnet/cli/issues/2143
-    $sharedPath = (Join-Path (Split-Path ((get-command dotnet.exe).Path) -Parent) "shared");
-    (Get-ChildItem $sharedPath -Recurse *dotnet.exe) | %{ $_.FullName } | Remove-Item;
-}
-
-function EnsureMSBuild() {
-    if(!(Test-Path $MSBuildDir)) {
-        try {
-            mkdir $MSBuildDir | Out-Null
-            $content = [IO.File]::ReadAllText((Convert-Path "$PSScriptRoot\msbuild.project.template.json"))
-            $content = $content.Replace("RUNTIME", $RID)
-
-            [IO.File]::WriteAllText((Join-Path $MSBuildDir "project.json"), $content);
-
-            copy "$KoreBuildRoot\NuGet.config" "$MSBuildDir"
-
-            Write-Host -ForegroundColor Green "Preparing MSBuild ..."
-            exec dotnet restore "$MSBuildDir\project.json" -v Detailed
-            exec dotnet publish "$MSBuildDir\project.json" -o "$MSBuildDir\bin\pub"
-
-            Write-Host -ForegroundColor Green "Preparing KoreBuild Tasks ..."
-            exec dotnet restore "$KoreBuildRoot\src\Microsoft.AspNetCore.Build" -v Detailed
-            exec dotnet publish "$KoreBuildRoot\src\Microsoft.AspNetCore.Build" -o "$MSBuildDir\bin\pub" -f "netcoreapp1.0"
-        } catch {
-            $ex = $error[0]
-            # Clean up to ensure we aren't half-initialized
-            if(Test-Path $MSBuildDir) {
-                del -rec -for $MSBuildDir
-            }
-
-            throw $ex
+<#  
+.SYNOPSIS
+    You can add this to you build script to ensure that psbuild is available before calling
+    Invoke-MSBuild. If psbuild is not available locally it will be downloaded automatically.
+#>
+function EnsurePsbuildInstalled{  
+    [cmdletbinding()]
+    param(
+        [string]$psbuildInstallUri = 'https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1'
+    )
+    process{
+        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
+            'Installing psbuild from [{0}]' -f $psbuildInstallUri | Write-Verbose
+            (new-object Net.WebClient).DownloadString($psbuildInstallUri) | iex
         }
-    } else {
-        Write-Host -ForegroundColor DarkGray "MSBuild already initialized, use -Reset to refresh it"
-    }
-}
-
-function EnsureTools() {
-    if(!(Test-Path $ToolsDir)) {
-        try {
-            Write-Host -ForegroundColor Green "Preparing other build tools ..."
-            exec dotnet restore "$KoreBuildRoot\tools\project.json" -v Detailed --packages "$ToolsDir"
-        } catch {
-            # Clean up to ensure we aren't half-initialized
-            if(Test-Path $ToolsDir) {
-                del -rec -for $ToolsDir
-            }
+        else{
+            'psbuild already loaded, skipping download' | Write-Verbose
         }
-    } else {
-        Write-Host -ForegroundColor DarkGray "Build tools already initialized, use -Reset to refresh them"
+
+        # make sure it's loaded and throw if not
+        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
+            throw ('Unable to install/load psbuild from [{0}]' -f $psbuildInstallUri)
+        }
     }
 }
 
-try {
-    EnsureDotNet
-    EnsureMSBuild
-    EnsureTools
+# Taken from psake https://github.com/psake/psake
 
-    $KoreBuildTargetsRoot = "$KoreBuildRoot\src\Microsoft.AspNetCore.Build\targets"
-
-    # Check for a local KoreBuild project
-    $Proj = Join-Path "$RepositoryRoot" "makefile.proj"
-    if(!(Test-Path $Proj)) {
-        $Proj = Join-Path "$KoreBuildTargetsRoot" "makefile.proj"
+<#  
+.SYNOPSIS
+  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+  to see if an error occcured. If an error is detected then an exception is thrown.
+  This function allows you to run command-line programs without having to
+  explicitly check the $lastexitcode variable.
+.EXAMPLE
+  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+function Exec  
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
     }
-
-    $MSBuildArguments = @"
-/nologo
-"$Proj"
-/p:KoreBuildToolsPackages="$ToolsDir"
-/p:KoreBuildTargetsPath="$KoreBuildTargetsRoot"
-/p:KoreBuildTasksPath="$MSBuildDir\bin\pub"
-/fl
-/flp:LogFile="$MSBuildLog";Verbosity=diagnostic;Encoding=UTF-8
-"@
-
-    $MSBuildArguments | Out-File -Encoding ASCII -FilePath $MSBuildResponseFile
-    $args | ForEach { $_ | Out-File -Append -Encoding ASCII -FilePath $MSBuildResponseFile } # Add local args to RSP
-
-    Write-Host -ForegroundColor Green "Starting build ..."
-    Write-Host -ForegroundColor DarkGray "> msbuild $Proj $args"
-
-    & "$MSBuildDir\bin\pub\CoreRun.exe" "$MSBuildDir\bin\pub\MSBuild.exe" `@"$MSBuildResponseFile"
-} finally {
-    # Copy logs to artifacts
-    if(!(Test-Path $ArtifactsDir)) {
-        mkdir $ArtifactsDir | Out-Null
-    }
-    cp -ErrorAction SilentlyContinue $MSBuildResponseFile,$KoreBuildLog,$MSBuildLog $ArtifactsDir
 }
+
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
+EnsurePsbuildInstalled
+
+exec { & dotnet restore }
+
+Invoke-MSBuild
+
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+
+exec { & dotnet test .\Promact.Oauth.Server\src\Promact.Oauth.Server.Tests -c Release -appveyor }
+
+exec { & dotnet pack .\Promact.Oauth.Server\src\Promact.Oauth.Server.Tests -c Release -o .\artifacts --version-suffix=$revision }  
