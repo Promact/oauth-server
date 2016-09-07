919fb508ccc55ee9453d009b06da1d8d4f1a9346
gourav <gourav@PR2C4R3Y2015>
2016-09-07 18:50:11 +0530

    added methods in project and user controller and respective repositories to respond to requests from erp server

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectController.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectController.cs
@@ -130,5 +130,13 @@ namespace Promact.Oauth.Server.Controllers
             List<UserAc> projectUsers =  _projectRepository.GetProjectUsersByTeamLeaderId(teamLeaderId);
             return projectUsers;
         }
+
+        [HttpGet]
+        [Route("projectUsersById/{teamLeaderId}")]
+        public  List<UserAc> GetProjectUsersByTeamLeaderId(string teamLeaderId)
+        {
+            List<UserAc> projectUsers =  _projectRepository.GetProjectUsersByTeamLeaderId(teamLeaderId);
+            return projectUsers;
+        }
     }
 }
