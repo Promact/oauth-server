6da453fc2a2f6dca462058f120ba427953c5af0f
ankit <ankit@PR1C2R4Y2015>
2016-09-07 12:09:09 +0530

    Updated user test cases

--- Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/ProjectTests.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/ProjectTests.cs
@@ -1,179 +1,179 @@
-﻿//using Microsoft.EntityFrameworkCore.Infrastructure;
-//using Microsoft.Extensions.DependencyInjection;
-//using Promact.Oauth.Server.Models.ApplicationClasses;
-//using Promact.Oauth.Server.Repository.ProjectsRepository;
-//using Promact.Oauth.Server.Data_Repository;
-//using Xunit;
-//using System;
-//using Promact.Oauth.Server.Models;
-//using System.Collections.Generic;
-//using System.Globalization;
-//using System.Threading.Tasks;
-//using Promact.Oauth.Server.Constants;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Promact.Oauth.Server.Models.ApplicationClasses;
+using Promact.Oauth.Server.Repository.ProjectsRepository;
+using Promact.Oauth.Server.Data_Repository;
+using Xunit;
+using System;
+using Promact.Oauth.Server.Models;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using Promact.Oauth.Server.Constants;
 
-//namespace Promact.Oauth.Server.Tests
-//{
-//    public class ProjectTests : BaseProvider
-//    {
-//        private readonly IProjectRepository _projectRepository;
+namespace Promact.Oauth.Server.Tests
+{
+    public class ProjectTests : BaseProvider
+    {
+        private readonly IProjectRepository _projectRepository;
 
-//        private readonly IDataRepository<Project> _dataRepository;
-//        private readonly IDataRepository<ProjectUser> _dataRepositoryProjectUser;
-      
-        
-//        public ProjectTests() : base()
-//        {
-//            _projectRepository = serviceProvider.GetService<IProjectRepository>();
-//            _dataRepository = serviceProvider.GetService<IDataRepository<Project>>();
-//            _dataRepositoryProjectUser = serviceProvider.GetService<IDataRepository<ProjectUser>>();
-//        }
+        private readonly IDataRepository<Project> _dataRepository;
+        private readonly IDataRepository<ProjectUser> _dataRepositoryProjectUser;
 
-//        ProjectAc projectac = new ProjectAc()
-//        {
-//            Name = StringConstant.Name,
-//            SlackChannelName = StringConstant.SlackChannelName,
-//            IsActive = StringConstant.IsActive,
-//            TeamLeader = new UserAc { FirstName = StringConstant.FirstName },
-//            TeamLeaderId = StringConstant.TeamLeaderId,
-//            CreatedBy = StringConstant.CreatedBy
 
-//        };
+        public ProjectTests() : base()
+        {
+            _projectRepository = serviceProvider.GetService<IProjectRepository>();
+            _dataRepository = serviceProvider.GetService<IDataRepository<Project>>();
+            _dataRepositoryProjectUser = serviceProvider.GetService<IDataRepository<ProjectUser>>();
+        }
 
-//        ProjectUser projectUser = new ProjectUser()
-//        {
-//            ProjectId = 1,
-//            Project = new Project { Name = StringConstant.Name },
-//            UserId = StringConstant.UserId,
-//            User = new ApplicationUser { FirstName = StringConstant.FirstName }
-//        };
+        ProjectAc projectac = new ProjectAc()
+        {
+            Name = StringConstant.Name,
+            SlackChannelName = StringConstant.SlackChannelName,
+            IsActive = StringConstant.IsActive,
+            TeamLeader = new UserAc { FirstName = StringConstant.FirstName },
+            TeamLeaderId = StringConstant.TeamLeaderId,
+            CreatedBy = StringConstant.CreatedBy
 
-//        UserAc user = new UserAc()
-//        {
+        };
 
-//            FirstName = StringConstant.FirstName,
-//            LastName = StringConstant.LastName,
-//            Email = StringConstant.Email,
-//            RoleName = StringConstant.Employee
-//        };
+        ProjectUser projectUser = new ProjectUser()
+        {
+            ProjectId = 1,
+            Project = new Project { Name = StringConstant.Name },
+            UserId = StringConstant.UserId,
+            User = new ApplicationUser { FirstName = StringConstant.FirstName }
+        };
 
-//        UserAc userSecound = new UserAc()
-//        { Id = StringConstant.UserIdSecond, FirstName = StringConstant.FirstNameSecond };
-//        UserAc userThird = new UserAc()
-//        { Id = StringConstant.UserIdThird, FirstName = StringConstant.FirstNameThird };
+        UserAc user = new UserAc()
+        {
 
-//        /// <summary>
-//        /// This test case to add a new project
-//        /// </summary>
-//        [Fact, Trait("Category", "Required")]
-//        public void AddProject()
-//        {
-//            Task<int> id = _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
-//            var project = _dataRepository.FirstOrDefault(x => x.Id == id.Result);
-//            Assert.NotNull(project);
-//        }
+            FirstName = StringConstant.FirstName,
+            LastName = StringConstant.LastName,
+            Email = StringConstant.Email,
+            RoleName = StringConstant.Employee
+        };
 
-//        /// <summary>
-//        /// This test case for the add user and project in userproject table
-//        /// </summary>
-//        [Fact, Trait("Category", "Required")]
-//        public void AddUserProject()
-//        {
-//            _projectRepository.AddUserProject(projectUser);
-//            var ProjectUser = _dataRepositoryProjectUser.Fetch(x => x.ProjectId == 1);
-//            Assert.NotNull(ProjectUser);
-//        }
-        
-//        /// <summary>
-//        /// This test case for gets project By Id
-//        /// </summary>
-//        [Fact, Trait("Category", "Required")]
-//        public void GetById()
-//        {
-//            projectac.TeamLeaderId = "1";
-//            Task<int> id = _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
-//            _projectRepository.AddUserProject(projectUser);
-//            Task<ProjectAc> project = _projectRepository.GetById(id.Result);
-//            Assert.NotNull(project);
-//        }
+        UserAc userSecound = new UserAc()
+        { Id = StringConstant.UserIdSecond, FirstName = StringConstant.FirstNameSecond };
+        UserAc userThird = new UserAc()
+        { Id = StringConstant.UserIdThird, FirstName = StringConstant.FirstNameThird };
 
-//        /// <summary>
-//        /// This test case edit project 
-//        /// </summary>
-//        [Fact, Trait("Category", "Required")]
-//        public void EditProject()
-//        {
-//            List<UserAc> userlist = new List<UserAc>();
-//            userlist.Add(user);
-//            userlist.Add(userSecound);
-//            userlist.Add(userThird);
-//            Task<int> id = _projectRepository.AddProject(projectac, StringConstant.FirstName);
-//            _projectRepository.AddUserProject(projectUser);
-//            ProjectAc projectacSecound = new ProjectAc()
-//            {
-//                Id = id.Result,
-//                Name = StringConstant.EditName,
-//                SlackChannelName = StringConstant.SlackChannelName,
-//                IsActive = StringConstant.IsActive,
-//                TeamLeader = new UserAc { FirstName = StringConstant.FirstName },
-//                TeamLeaderId = StringConstant.TeamLeaderId,
-//                CreatedBy = StringConstant.CreatedBy,
-//                CreatedDate = DateTime.Now.ToString(CultureInfo.InvariantCulture),
-//                ApplicationUsers = userlist
-//            };
-//            _projectRepository.EditProject(projectacSecound, StringConstant.CreatedBy);
-//            var project = _dataRepository.Fetch(x => x.Id == 1);
-//            _dataRepositoryProjectUser.Fetch(x => x.ProjectId == 1);
-//            Assert.NotNull(project);
-//        }
+        /// <summary>
+        /// This test case to add a new project
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void AddProject()
+        {
+            Task<int> id = _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
+            var project = _dataRepository.FirstOrDefault(x => x.Id == id.Result);
+            Assert.NotNull(project);
+        }
 
-//        /// <summary>
-//        /// This test case for the check duplicate project
-//        /// </summary>
-//        [Fact, Trait("Category", "Required")]
-//        public void checkDuplicateNegative()
-//        {
-//            _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
-//            var project = _projectRepository.checkDuplicate(projectac);
-//            Assert.Null(project.Name);
-//        }
+        /// <summary>
+        /// This test case for the add user and project in userproject table
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void AddUserProject()
+        {
+            _projectRepository.AddUserProject(projectUser);
+            var ProjectUser = _dataRepositoryProjectUser.Fetch(x => x.ProjectId == 1);
+            Assert.NotNull(ProjectUser);
+        }
 
-//        /// <summary>
-//        /// This test case for the check duplicate project
-//        /// </summary>
-//        [Fact, Trait("Category", "Required")]
-//        public void checkDuplicatePositive()
-//        {
-//            _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
-//            List<UserAc> userlist = new List<UserAc>();
-//            userlist.Add(userSecound);
-//            userlist.Add(userThird);
-//            ProjectAc projectacSecound = new ProjectAc()
-//            {
-//                Id = 2,
-//                Name = StringConstant.ProjectName,
-//                SlackChannelName = StringConstant.SlackChannelName,
-//                IsActive = true,
-//                TeamLeader = new UserAc { FirstName = StringConstant.FirstName },
-//                TeamLeaderId = StringConstant.TeamLeaderId,
-//                CreatedBy = StringConstant.CreatedBy,
-//                CreatedDate = DateTime.Now.ToString(CultureInfo.InvariantCulture),
-//                ApplicationUsers = userlist
-//            };
-//            _projectRepository.AddProject(projectacSecound, StringConstant.CreatedBy);
-//            var project = _projectRepository.checkDuplicate(projectacSecound);
-//            Assert.Null(project.Name);
-//        }
+        /// <summary>
+        /// This test case for gets project By Id
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void GetById()
+        {
+            projectac.TeamLeaderId = "1";
+            Task<int> id = _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
+            _projectRepository.AddUserProject(projectUser);
+            Task<ProjectAc> project = _projectRepository.GetById(id.Result);
+            Assert.NotNull(project);
+        }
 
-//        /// <summary>
-//        /// This test case for the get all projects
-//        /// </summary>
-//        [Fact, Trait("Category", "Required")]
-//        public void GetAllProject()
-//        {
-//            _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
-//            _projectRepository.AddUserProject(projectUser);
-//            Task<IEnumerable<ProjectAc>> projects = _projectRepository.GetAllProjects();
-//            Assert.NotNull(projects);
-//        }
-//    }
-//}
+        /// <summary>
+        /// This test case edit project 
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void EditProject()
+        {
+            List<UserAc> userlist = new List<UserAc>();
+            userlist.Add(user);
+            userlist.Add(userSecound);
+            userlist.Add(userThird);
+            Task<int> id = _projectRepository.AddProject(projectac, StringConstant.FirstName);
+            _projectRepository.AddUserProject(projectUser);
+            ProjectAc projectacSecound = new ProjectAc()
+            {
+                Id = id.Result,
+                Name = StringConstant.EditName,
+                SlackChannelName = StringConstant.SlackChannelName,
+                IsActive = StringConstant.IsActive,
+                TeamLeader = new UserAc { FirstName = StringConstant.FirstName },
+                TeamLeaderId = StringConstant.TeamLeaderId,
+                CreatedBy = StringConstant.CreatedBy,
+                CreatedDate = DateTime.Now.ToString(CultureInfo.InvariantCulture),
+                ApplicationUsers = userlist
+            };
+            _projectRepository.EditProject(projectacSecound, StringConstant.CreatedBy);
+            var project = _dataRepository.Fetch(x => x.Id == 1);
+            _dataRepositoryProjectUser.Fetch(x => x.ProjectId == 1);
+            Assert.NotNull(project);
+        }
+
+        /// <summary>
+        /// This test case for the check duplicate project
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void checkDuplicateNegative()
+        {
+            _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
+            var project = _projectRepository.checkDuplicate(projectac);
+            Assert.Null(project.Name);
+        }
+
+        /// <summary>
+        /// This test case for the check duplicate project
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void checkDuplicatePositive()
+        {
+            _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
+            List<UserAc> userlist = new List<UserAc>();
+            userlist.Add(userSecound);
+            userlist.Add(userThird);
+            ProjectAc projectacSecound = new ProjectAc()
+            {
+                Id = 2,
+                Name = StringConstant.ProjectName,
+                SlackChannelName = StringConstant.SlackChannelName,
+                IsActive = true,
+                TeamLeader = new UserAc { FirstName = StringConstant.FirstName },
+                TeamLeaderId = StringConstant.TeamLeaderId,
+                CreatedBy = StringConstant.CreatedBy,
+                CreatedDate = DateTime.Now.ToString(CultureInfo.InvariantCulture),
+                ApplicationUsers = userlist
+            };
+            _projectRepository.AddProject(projectacSecound, StringConstant.CreatedBy);
+            var project = _projectRepository.checkDuplicate(projectacSecound);
+            Assert.Null(project.Name);
+        }
+
+        /// <summary>
+        /// This test case for the get all projects
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void GetAllProject()
+        {
+            _projectRepository.AddProject(projectac, StringConstant.CreatedBy);
+            _projectRepository.AddUserProject(projectUser);
+            Task<IEnumerable<ProjectAc>> projects = _projectRepository.GetAllProjects();
+            Assert.NotNull(projects);
+        }
+    }
+}
--- Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/UserRepositoryTest.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/UserRepositoryTest.cs
@@ -30,67 +30,67 @@ namespace Promact.Oauth.Server.Tests
 
         #region Test Case
 
-        ///// <summary>
-        ///// This test case gets the list of all users
-        ///// </summary>
-        //[Fact, Trait("Category", "Required")]
-        //public void GetAllUser()
-        //{
-        //    //AddRole();
-        //    _userRepository.AddUser(_testUser, "Rajdeep");
-        //    IEnumerable<UserAc> users = _userRepository.GetAllUsers().Result;
-        //    Assert.NotNull(users);
-        //}
+        /// <summary>
+        /// This test case gets the list of all users
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void GetAllUser()
+        {
+            AddRole().Wait();
+            var id = _userRepository.AddUser(_testUser, "Rajdeep").Result;
+            IEnumerable<UserAc> users = _userRepository.GetAllUsers().Result;
+            Assert.NotNull(users);
+        }
 
-        ///// <summary>
-        ///// This test case gets the user by its id
-        ///// </summary>
-        //[Fact, Trait("Category", "Required")]
-        //public void GetUserById()
-        //{
+        /// <summary>
+        /// This test case gets the user by its id
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void GetUserById()
+        {
 
-        //    UserAc user = new UserAc()
-        //    {
-        //        Email = "testUser2@promactinfo.com",
-        //        FirstName = "First name 2",
-        //        LastName = "Last name 2",
-        //        IsActive = true,
-        //        Password = "User@123",
-        //        UserName = "testUser2@promactinfo.com",
-        //        SlackUserName = "test",
-        //        RoleName = StringConstant.RoleName
-        //    };
-        //    //AddRole();
-        //    var id = _userRepository.AddUser(user, "Rajdeep");
-        //    UserAc testUser = _userRepository.GetById(id).Result;
+            UserAc user = new UserAc()
+            {
+                Email = "testUser2@promactinfo.com",
+                FirstName = "First name 2",
+                LastName = "Last name 2",
+                IsActive = true,
+                Password = "User@123",
+                UserName = "testUser2@promactinfo.com",
+                SlackUserName = "test",
+                RoleName = StringConstant.Employee
+            };
+            AddRole().Wait();
+            var id = _userRepository.AddUser(user, "Rajdeep").Result;
+            UserAc testUser = _userRepository.GetById(id).Result;
 
-        //    Assert.NotNull(testUser);
-        //}
+            Assert.NotNull(testUser);
+        }
 
 
-        ///// <summary>
-        ///// This test case checks if a user exists with the specified Email
-        ///// </summary>
-        //[Fact, Trait("Category", "Required")]
-        //public void FindByEmail()
-        //{
-        //    //AddRole();
-        //    var result = _userRepository.AddUser(_testUser, "Rajdeep");
-        //    var exists = _userRepository.FindByEmail("testUser@promactinfo.com");
-        //    Assert.Equal(true, exists);
-        //}
+        /// <summary>
+        /// This test case checks if a user exists with the specified Email
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void FindByEmail()
+        {
+            AddRole().Wait();
+            var result = _userRepository.AddUser(_testUser, "Rajdeep").Result;
+            var exists = _userRepository.FindByEmail("testUser@promactinfo.com");
+            Assert.Equal(true, exists);
+        }
 
-        ///// <summary>
-        ///// This test case checks if a user exists with the specified UserName
-        ///// </summary>
-        //[Fact, Trait("Category", "Required")]
-        //public void FindByUserName()
-        //{
-        //    //AddRole();
-        //    _userRepository.AddUser(_testUser, "Rajdeep");
-        //    var exists = _userRepository.FindByUserName("testUser@promactinfo.com");
-        //    Assert.Equal(true, exists);
-        //}
+        /// <summary>
+        /// This test case checks if a user exists with the specified UserName
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void FindByUserName()
+        {
+            AddRole().Wait();
+            var id = _userRepository.AddUser(_testUser, "Rajdeep").Result;
+            var exists = _userRepository.FindByUserName("testUser@promactinfo.com");
+            Assert.Equal(true, exists);
+        }
 
         /// <summary>
         /// This test case is used for adding new user
@@ -104,15 +104,17 @@ namespace Promact.Oauth.Server.Tests
             Assert.NotNull(user);
         }
 
-        ///// <summary>
-        ///// This test case is used for updating user details
-        ///// </summary>
+        /// <summary>
+        /// This test case is used for updating user details
+        /// </summary>
         //[Fact, Trait("Category", "Required")]
         //public void UpdateUser()
         //{
-        //    //AddRole();
-        //    _userRepository.AddUser(_testUser, "Rajdeep");
-        //    var user = _dataRepository.FirstOrDefault(u => u.Email == "testUser@promactinfo.com");
+        //    AddRole().Wait();
+        //    //_userRepository.AddUser(_testUser, "Rajdeep");
+        //    var userId = _userRepository.AddUser(_testUser, "Rajdeep").Result;
+        //    var user = _userManager.FindByIdAsync(userId).Result;
+        //    //var user = _dataRepository.FirstOrDefault(u => u.Email == "testUser@promactinfo.com");
 
         //    string id = _userRepository.UpdateUserDetails(new UserAc
         //    {
@@ -122,19 +124,20 @@ namespace Promact.Oauth.Server.Tests
         //        RoleName = "Employee"
         //    }, "Rajdeep");
 
-        //    var editedUser = _dataRepository.FirstOrDefault(u => u.Id == id);
+        //    var editedUser = _userManager.FindByIdAsync(id).Result;
         //    Assert.Equal("Updated User", editedUser.FirstName);
         //}
 
-        ///// <summary>
-        ///// This test case is used for changing the password of an user
-        ///// </summary>
+        /// <summary>
+        /// This test case is used for changing the password of an user
+        /// </summary>
         //[Fact, Trait("Category", "Required")]
         //public void ChangePassword()
         //{
-        //    //AddRole();
-        //    _userRepository.AddUser(_testUser, "Rajdeep");
-        //    var user = _dataRepository.FirstOrDefault(u => u.Email == "testUser@promactinfo.com");
+        //    AddRole().Wait();
+        //    var id = _userRepository.AddUser(_testUser, "Rajdeep").Result;
+        //    var user = _userManager.FindByIdAsync(id).Result;
+        //    //var user = _dataRepository.FirstOrDefault(u => u.Email == "testUser@promactinfo.com");
 
         //    var password = _userRepository.ChangePassword(new ChangePasswordViewModel
         //    {
@@ -147,42 +150,42 @@ namespace Promact.Oauth.Server.Tests
         //    Assert.Equal(true, passwordMatch);
         //}
 
-        /////// <summary>
-        /////// Test case use for getting user details by its first name
-        /////// </summary>
-        ////[Fact, Trait("Category", "Required")]
-        ////public void UserDetialByFirstName()
-        ////{
-        ////    //AddRole();
-        ////    string id = _userRepository.AddUser(_testUser, "Siddhartha");
-        ////    var user = _userRepository.UserDetialByFirstName("First name");
-        ////    Assert.Equal(user.Email, _testUser.Email);
-        ////}
-
         ///// <summary>
-        ///// Test case use for getting team leader's details by users first name
+        ///// Test case use for getting user details by its first name
         ///// </summary>
         //[Fact, Trait("Category", "Required")]
-        //public void TeamLeaderByUserId()
+        //public void UserDetialByFirstName()
         //{
         //    //AddRole();
         //    string id = _userRepository.AddUser(_testUser, "Siddhartha");
-        //    var user = _userRepository.TeamLeaderByUserId("First name").Result;
-        //    Assert.Equal(0, user.Count);
+        //    var user = _userRepository.UserDetialByFirstName("First name");
+        //    Assert.Equal(user.Email, _testUser.Email);
         //}
 
-        ///// <summary>
-        ///// Test case use for getting management's details by users first name
-        ///// </summary>
-        //[Fact, Trait("Category", "Required")]
-        //public void ManagementByUserId()
-        //{
-        //    // AddRole();
-        //    string id = _userRepository.AddUser(_testUser, "Siddhartha");
-        //    var user = _userRepository.ManagementByUserId().Result;
-        //    Assert.Equal(0, user.Count);
-        //}
-        
+        /// <summary>
+        /// Test case use for getting team leader's details by users first name
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void TeamLeaderByUserId()
+        {
+            AddRole().Wait();
+            string id = _userRepository.AddUser(_testUser, "Siddhartha").Result;
+            var user = _userRepository.TeamLeaderByUserId("First name").Result;
+            Assert.Equal(0, user.Count);
+        }
+
+        /// <summary>
+        /// Test case use for getting management's details by users first name
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void ManagementByUserId()
+        {
+            AddRole().Wait();
+            string id = _userRepository.AddUser(_testUser, "Siddhartha").Result;
+            var user = _userRepository.ManagementByUserId().Result;
+            Assert.Equal(0, user.Count);
+        }
+
         #endregion
 
         private async Task AddRole()
