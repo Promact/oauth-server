c7b4e42ce97f57ee11d3dc050b653569762fe2cc
Siddhartha <Siddhartha@PR2C5R2Y2015>
2016-09-08 18:43:31 +0530

    Added Add User and Get All User test case to check for build in travis CI

--- Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/UserRepositoryTest.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server.Tests/UserRepositoryTest.cs
@@ -33,14 +33,14 @@ namespace Promact.Oauth.Server.Tests
         /// <summary>
         /// This test case gets the list of all users
         /// </summary>
-        //[Fact, Trait("Category", "Required")]
-        //public void GetAllUser()
-        //{
-        //    AddRole().Wait();
-        //    var id = _userRepository.AddUser(_testUser, "Rajdeep").Result;
-        //    IEnumerable<UserAc> users = _userRepository.GetAllUsers();
-        //    Assert.NotNull(users);
-        //}
+        [Fact, Trait("Category", "Required")]
+        public void GetAllUser()
+        {
+            AddRole().Wait();
+            var id = _userRepository.AddUser(_testUser, StringConstant.RawFirstNameForTest).Result;
+            IEnumerable<UserAc> users = _userRepository.GetAllUsers();
+            Assert.Equal(users.Count(),1);
+        }
 
         //        ///// <summary>
         //        ///// This test case gets the user by its id
@@ -251,37 +251,36 @@ namespace Promact.Oauth.Server.Tests
         //    Assert.Equal(8,casualLeave);
         //}
         #endregion
-        //private async Task AddRole()
-        //{
-        //    if (!_roleManager.Roles.Any())
-        //    {
-        //        List<IdentityRole> roles = new List<IdentityRole>();
-        //        roles.Add(new IdentityRole { Name = StringConstant.Employee, NormalizedName = StringConstant.NormalizedName });
-        //        roles.Add(new IdentityRole { Name = StringConstant.Admin, NormalizedName = StringConstant.NormalizedSecond });
+        private async Task AddRole()
+        {
+            if (!_roleManager.Roles.Any())
+            {
+                List<IdentityRole> roles = new List<IdentityRole>();
+                roles.Add(new IdentityRole { Name = StringConstant.Employee, NormalizedName = StringConstant.NormalizedName });
+                roles.Add(new IdentityRole { Name = StringConstant.Admin, NormalizedName = StringConstant.NormalizedSecond });
 
-        //        foreach (var role in roles)
-        //        {
-        //            var roleExit = await _roleManager.RoleExistsAsync(role.Name);
-        //            if (!roleExit)
-        //            {
-        //                var result = await _roleManager.CreateAsync(role);
-        //            }
-        //        }
-        //    }
-        //}
+                foreach (var role in roles)
+                {
+                    var roleExit = await _roleManager.RoleExistsAsync(role.Name);
+                    if (!roleExit)
+                    {
+                        var result = await _roleManager.CreateAsync(role);
+                    }
+                }
+            }
+        }
 
-        //private UserAc _testUser = new UserAc()
-        //{
-        //    Email = "testUser@promactinfo.com",
-        //    FirstName = "First name",
-        //    LastName = "Last name",
-        //    IsActive = true,
-        //    Password = "User@123",
-        //    UserName = "testUser@pronactinfo.com",
-        //    SlackUserName = "test",
-        //    JoiningDate = DateTime.UtcNow,
-        //    RoleName = StringConstant.Employee
-        //};
+        private UserAc _testUser = new UserAc()
+        {
+            Email = StringConstant.RawEmailIdForTest,
+            FirstName = StringConstant.RawFirstNameForTest,
+            LastName = StringConstant.RawLastNameForTest,
+            IsActive = true,
+            UserName = StringConstant.RawEmailIdForTest,
+            SlackUserName = StringConstant.RawFirstNameForTest,
+            JoiningDate = DateTime.UtcNow,
+            RoleName = StringConstant.Employee
+        };
 
 
         //private UserAc userLocal = new UserAc()
