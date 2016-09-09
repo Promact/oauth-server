f0938182fc865b4cb4419335ae48398df04c02ea
ankit <ankit@PR1C2R4Y2015>
2016-09-09 10:43:33 +0530

    Update gulpfile.js and karma.conf.js and travis.yml for setup check clcient side tes case

--- Promact.Oauth.Server/src/Promact.Oauth.Server/gulpfile.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/gulpfile.js
@@ -58,14 +58,13 @@ gulp.task("copytowwwroot", function () {
 
 gulp.task('bundle', function (done) {
     var builder = new sysBuilder('./wwwroot', './wwwroot/systemjs.config.js');
-
     builder
      .buildStatic('app', './wwwroot/bundle.js', {
       runtime: false
   }).then(function () {
       done();
-  })
-   
+  });
+
 });
 
 gulp.task("clean:js", function (cb) {
@@ -93,14 +92,6 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
-
-//// Run test once and exit
-//gulp.task('test', function (done) {
-//    new Server({
-//        configFile: __dirname + '/karma.conf.js',
-//        singleRun: true
-//    }, done).start();
-//});
 
 //Runs karma test
 gulp.task('test', function (done) {
