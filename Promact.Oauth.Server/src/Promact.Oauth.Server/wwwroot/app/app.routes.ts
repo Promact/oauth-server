86b584519664ae3077dce055b72ff87731e92316
ankit <ankit@PR1C2R4Y2015>
2016-09-13 11:43:05 +0530

    rebase with dev branch and solve the conflicts

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.routes.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.routes.ts
@@ -5,7 +5,8 @@ import {Routes, RouterModule } from '@angular/router';
 import {ConsumerAppComponent} from './consumerapp/consumerapp.component';
 import {ProjectComponent} from "./project/project.component";
 import {UserComponent} from './users/user.component';
-
+import {changePasswordRoute} from './change-password/change-password.routes';
+import {ChangePassword} from './change-password/changepassword.component';
 
 const appRoutes: Routes = [
 
@@ -16,6 +17,8 @@ const appRoutes: Routes = [
     { path: 'project', component: ProjectComponent },
     ...userRoutes,
     { path: 'user', component: UserComponent },
+    ...changePasswordRoute,
+    { path: 'changepassword', component: ChangePassword },
 ]
 
 export const routing = RouterModule.forRoot(appRoutes);
