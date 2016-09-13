bb9e8e66b37f2fa0c014a8f9958128652f1cc9a3
ankit <ankit@PR1C2R4Y2015>
2016-09-13 17:09:20 +0530

    update karma.conf.js file and also update mock.project.service.ts and mock.user.service.ts

--- Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
           { pattern: 'node_modules/lodash/*.js', included: false, watched: true },
           { pattern: 'wwwroot/app/**/*.js', included: false, watched: true },
           { pattern: 'wwwroot/app/**/*.html', included: false, watched: true },
-          { pattern: 'wwwroot/app/users/User.model.js', included: false, watched: true },
+          //{ pattern: 'wwwroot/app/users/user.model.js', included: false, watched: true },
           { pattern: 'wwwroot/app/project/project-list/Project-list.Component.js', included: false, watched: true },
           { pattern: 'wwwroot/app/users/user-list/user-list.Component.js', included: false, watched: true },
           { pattern: 'wwwroot/app/users/user-list/user-list.Component.js.map', included: false, watched: true },
