9686f9a07595ccd2027d70e560ed64398993465e
ankit <ankit@PR1C2R4Y2015>
2016-09-14 15:43:40 +0530

    upgrade phatomjs version to 2.1.2

--- Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
@@ -100,9 +100,9 @@ module.exports = function (config) {
         concurrency: Infinity,
 
         customLaunchers: {
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
+            'PhantomJS_flags': {
+                base: 'PhantomJS',
+                flags: ['--load-images=false']
             }
         },
 
@@ -115,7 +115,7 @@ module.exports = function (config) {
     })
 
     if (process.env.TRAVIS || process.env.CIRCLECI) {
-        config.browsers = ['Chrome_travis_ci'];
+        config.browsers = ['PhantomJS_flags'];
         config.singleRun = true;
         config.browserNoActivityTimeout = 90000;
     }
