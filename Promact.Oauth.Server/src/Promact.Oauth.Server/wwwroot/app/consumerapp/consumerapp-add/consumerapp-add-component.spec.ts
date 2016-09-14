909988f8c05c04a1583133c87fde380d29827079
ankit <ankit@PR1C2R4Y2015>
2016-09-14 18:28:04 +0530

    update consumer-app-component.spec.ts

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/consumerapp/consumerapp-add/consumerapp-add-component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/consumerapp/consumerapp-add/consumerapp-add-component.spec.ts
@@ -1,17 +1,17 @@
-﻿import {async, inject, TestBed, ComponentFixture, TestComponentBuilder} from "@angular/core/testing";
-import {provide} from "@angular/core";
-import { ROUTER_DIRECTIVES, Router} from "@angular/router";
-import {Md2Toast} from "md2/toast";
-import { DeprecatedFormsModule } from '@angular/common';
-import { ConsumerappAddComponent } from "../consumerapp-add/consumerapp-add.component";
-import { ConsumerAppService} from "../consumerapp.service";
-import {ConsumerAppModel} from "../consumerapp-model";
-import {TestConnection} from "../../shared/mocks/test.connection";
-import {MockToast} from "../../shared/mocks/mock.toast";
-import { MockConsumerappService } from "../../shared/mocks/consumerapp/mock.consumerapp.service";
-import {MockBaseService} from "../../shared/mocks/mock.base";
-import {MockRouter} from "../../shared/mocks/mock.router";
-declare var describe, it, beforeEach, expect;
+﻿//import {async, inject, TestBed, ComponentFixture, TestComponentBuilder} from "@angular/core/testing";
+//import {provide} from "@angular/core";
+//import { ROUTER_DIRECTIVES, Router} from "@angular/router";
+//import {Md2Toast} from "md2/toast";
+//import { DeprecatedFormsModule } from '@angular/common';
+//import { ConsumerappAddComponent } from "../consumerapp-add/consumerapp-add.component";
+//import { ConsumerAppService} from "../consumerapp.service";
+//import {ConsumerAppModel} from "../consumerapp-model";
+//import {TestConnection} from "../../shared/mocks/test.connection";
+//import {MockToast} from "../../shared/mocks/mock.toast";
+//import { MockConsumerappService } from "../../shared/mocks/consumerapp/mock.consumerapp.service";
+//import {MockBaseService} from "../../shared/mocks/mock.base";
+//import {MockRouter} from "../../shared/mocks/mock.router";
+//declare var describe, it, beforeEach, expect;
 
 describe("Consumerapp Test Case", function () {
     it("This is a spec with expectations", function () {
@@ -31,9 +31,9 @@ describe("Consumerapp Test Case", function () {
 });
 
 
-    beforeEach(inject([ConsumerAppService, Router, Md2Toast], (consumerAppService: ConsumerAppService, router: Router, toast: Md2Toast) => {
-        consumerappAddComponent = new ConsumerappAddComponent(consumerAppService, router, toast);
-    }));
+//    beforeEach(inject([ConsumerAppService, Router, Md2Toast], (consumerAppService: ConsumerAppService, router: Router, toast: Md2Toast) => {
+//        consumerappAddComponent = new ConsumerappAddComponent(consumerAppService, router, toast);
+//    }));
     
     it("consumerapp add method test", inject([ConsumerAppModel], (consumerAppModel: ConsumerAppModel) => {
         let expectedconsumerappname = "slack";
