e1ef086ed465e850eefcfef1407d2e8b35a0a419
ankit <ankit@PR1C2R4Y2015>
2016-09-14 18:58:47 +0530

    update all component.spec.ts file and karma.conf.js file

--- Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
@@ -52,24 +52,24 @@ module.exports = function (config) {
         ],
 
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-            'wwwroot/app/**/!(*spec).js': ['coverage']
-        },
+        //// preprocess matching files before serving them to the browser
+        //// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        //preprocessors: {
+        //    'wwwroot/app/**/!(*spec).js': ['coverage']
+        //},
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'dots', 'coverage'],
+        reporters: ['progress'],
 
-        coverageReporter: {
-            reporters: [
-                { type: 'json', subdir: '.', file: 'coverage-final.json' },
-                { type: 'html', dir: 'coverage/', file: 'coverage.html'}
-            ]
-        },
+        //coverageReporter: {
+        //    reporters: [
+        //        { type: 'json', subdir: '.', file: 'coverage-final.json' },
+        //        { type: 'html', dir: 'coverage/', file: 'coverage.html'}
+        //    ]
+        //},
 
         // web server port
         port: 9876,
