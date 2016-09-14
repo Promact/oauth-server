ed6e43cd2b47f113aeda9ce9d99da127aa4d71f3
ankit <ankit@PR1C2R4Y2015>
2016-09-14 18:19:24 +0530

    update consumerapp-add.component.ts

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/consumerapp/consumerapp-add/consumerapp-add.component.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/consumerapp/consumerapp-add/consumerapp-add.component.ts
@@ -1,37 +1,37 @@
-﻿import {Component} from "@angular/core";
-import {ConsumerAppModel} from "../consumerapp-model";
-import { ROUTER_DIRECTIVES, Router } from "@angular/router";
-import { ConsumerAppService} from "../consumerapp.service";
-import {Md2Toast} from "md2/toast";
+﻿//import {Component} from "@angular/core";
+//import {ConsumerAppModel} from "../consumerapp-model";
+//import { ROUTER_DIRECTIVES, Router } from "@angular/router";
+//import { ConsumerAppService} from "../consumerapp.service";
+//import {Md2Toast} from "md2/toast";
 
-@Component({
-    templateUrl: "app/consumerapp/consumerapp-add/consumerapp-add.html",
-    directives: [],
-    providers: [Md2Toast]
-})
-export class ConsumerappAddComponent {
-    consumerModel: ConsumerAppModel;
-    constructor(private consumerAppService: ConsumerAppService, private router: Router ,private toast: Md2Toast) {
-        this.consumerModel = new ConsumerAppModel();
-    }
+//@Component({
+//    templateUrl: "app/consumerapp/consumerapp-add/consumerapp-add.html",
+//    directives: [],
+//    providers: [Md2Toast]
+//})
+//export class ConsumerappAddComponent {
+//    consumerModel: ConsumerAppModel;
+//    constructor(private consumerAppService: ConsumerAppService, private router: Router ,private toast: Md2Toast) {
+//        this.consumerModel = new ConsumerAppModel();
+//    }
 
-    submitApps(consumerModel) {
-        this.consumerAppService.addConsumerApps(consumerModel).subscribe((result) => {
-            if (result == true) {
-                this.toast.show('Consumer App is added successfully.');
-                this.cancel();
-            }
-            else if (result == false) {
-                this.toast.show('Consumer App Name is already exists.');
-            }
-        }, err => {
+//    submitApps(consumerModel) {
+//        this.consumerAppService.addConsumerApps(consumerModel).subscribe((result) => {
+//            if (result == true) {
+//                this.toast.show('Consumer App is added successfully.');
+//                this.cancel();
+//            }
+//            else if (result == false) {
+//                this.toast.show('Consumer App Name is already exists.');
+//            }
+//        }, err => {
 
-        });
-    }
+//        });
+//    }
 
-    cancel() {
-        this.router.navigate(['/consumerapp']);
-    }
+//    cancel() {
+//        this.router.navigate(['/consumerapp']);
+//    }
 
    
-}
+//}
