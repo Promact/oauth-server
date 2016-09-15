8b8266735b251b11c990969659c113b9be9b8038
ankit <ankit@PR1C2R4Y2015>
2016-09-15 17:53:12 +0530

    update all test case and karma.conf.js and package.json

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/consumerapp/consumerapp-add/consumerapp-add-component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/consumerapp/consumerapp-add/consumerapp-add-component.spec.ts
@@ -13,15 +13,6 @@ import {MockBaseService} from "../../shared/mocks/mock.base";
 import {MockRouter} from "../../shared/mocks/mock.router";
 declare var describe, it, beforeEach, expect;
 
-//describe("Consumerapp Test Case", function () {
-//    it("This is a spec with expectations", function () {
-//        var expectedValue = 12;
-//        var actualValue = 12;
-//        expect(expectedValue).toBe(actualValue);
-//    });
-//});
-
-
 describe("Consumerapp Test Case", function () {
     it("This is a spec with expectations", function () {
         var expectedValue = 12;
@@ -34,23 +25,23 @@ describe("Consumerapp Test Case", function () {
 describe("Consumerapp Add Test Case", () => {
     let consumerappAddComponent: ConsumerappAddComponent;
   
-//    beforeEach(() => {
-//        TestBed.configureTestingModule({
-//            providers: [
-//                provide(Router, { useClass: MockRouter }),
-//                provide(TestConnection, {useClass : TestConnection }),
-//                provide(ConsumerAppService, { useClass: MockConsumerappService }),
-//                provide(Md2Toast, { useClass: MockToast }),
-//                provide(MockBaseService, { useClass: MockBaseService }),
-//                provide(ConsumerAppModel, { useClass: ConsumerAppModel })
-//            ]
-//        });
-//    });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                provide(Router, { useClass: MockRouter }),
+                provide(TestConnection, {useClass : TestConnection }),
+                provide(ConsumerAppService, { useClass: MockConsumerappService }),
+                provide(Md2Toast, { useClass: MockToast }),
+                provide(MockBaseService, { useClass: MockBaseService }),
+                provide(ConsumerAppModel, { useClass: ConsumerAppModel })
+            ]
+        });
+    });
 
 
-//    beforeEach(inject([ConsumerAppService, Router, Md2Toast], (consumerAppService: ConsumerAppService, router: Router, toast: Md2Toast) => {
-//        consumerappAddComponent = new ConsumerappAddComponent(consumerAppService, router, toast);
-//    }));
+    beforeEach(inject([ConsumerAppService, Router, Md2Toast], (consumerAppService: ConsumerAppService, router: Router, toast: Md2Toast) => {
+        consumerappAddComponent = new ConsumerappAddComponent(consumerAppService, router, toast);
+    }));
     
     it("consumerapp add method test", inject([ConsumerAppModel], (consumerAppModel: ConsumerAppModel) => {
         let expectedconsumerappname = "slack";
