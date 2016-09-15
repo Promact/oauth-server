d40c8808b785618bd55814a25c073e36f8760c4d
ankit <ankit@PR1C2R4Y2015>
2016-09-15 17:53:12 +0530

    update all test case and karma.conf.js and package.json

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user-add/user-add.component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user-add/user-add.component.spec.ts
@@ -1,61 +1,61 @@
-﻿//declare var describe, it, beforeEach, expect;
-//import {async, inject, TestBed, ComponentFixture, TestComponentBuilder} from '@angular/core/testing';
-//import {provide} from "@angular/core";
-//import {UserAddComponent} from "../user-add/user-add.component";
-//import { DeprecatedFormsModule } from '@angular/common';
-//import {UserService} from "../user.service";
-//import {UserModel} from '../../users/user.model';
-//import {ROUTER_DIRECTIVES, Router, ActivatedRoute} from '@angular/router';
-//import {Md2Toast} from 'md2/toast';
-//import {MockToast} from "../../shared/mocks/mock.toast";
-//import {Md2Multiselect } from 'md2/multiselect';
-//import {TestConnection} from "../../shared/mocks/test.connection";
-//import {MockUserService} from "../../shared/mocks/user/mock.user.service";
-//import {MockBaseService} from '../../shared/mocks/mock.base';
-//import {MockRouter} from '../../shared/mocks/mock.router';
+﻿declare var describe, it, beforeEach, expect;
+import {async, inject, TestBed, ComponentFixture, TestComponentBuilder} from '@angular/core/testing';
+import {provide} from "@angular/core";
+import {UserAddComponent} from "../user-add/user-add.component";
+import { DeprecatedFormsModule } from '@angular/common';
+import {UserService} from "../user.service";
+import {UserModel} from '../../users/user.model';
+import {ROUTER_DIRECTIVES, Router, ActivatedRoute} from '@angular/router';
+import {Md2Toast} from 'md2/toast';
+import {MockToast} from "../../shared/mocks/mock.toast";
+import {Md2Multiselect } from 'md2/multiselect';
+import {TestConnection} from "../../shared/mocks/test.connection";
+import {MockUserService} from "../../shared/mocks/user/mock.user.service";
+import {MockBaseService} from '../../shared/mocks/mock.base';
+import {MockRouter} from '../../shared/mocks/mock.router';
 
 
-//describe('User Add Test', () => {
-//    let userAddComponent: UserAddComponent;
+describe('User Add Test', () => {
+    let userAddComponent: UserAddComponent;
     
-//    class MockActivatedRoute { }
+    class MockActivatedRoute { }
 
-//    beforeEach(() => {
-//        TestBed.configureTestingModule({
-//            providers: [
-//                { provide: ActivatedRoute, useClass: MockActivatedRoute },
-//                { provide: Router, useClass: MockRouter },
-//                { provide: TestConnection, useClass: TestConnection },
-//                { provide: UserService, useClass: MockUserService },
-//                { provide: Md2Toast, useClass: MockToast },
-//                { provide: MockBaseService, useClass: MockBaseService },
-//                { provide: UserModel, useClass: UserModel }
-//            ]
-//        });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: ActivatedRoute, useClass: MockActivatedRoute },
+                { provide: Router, useClass: MockRouter },
+                { provide: TestConnection, useClass: TestConnection },
+                { provide: UserService, useClass: MockUserService },
+                { provide: Md2Toast, useClass: MockToast },
+                { provide: MockBaseService, useClass: MockBaseService },
+                { provide: UserModel, useClass: UserModel }
+            ]
+        });
 
-//    });
-//    beforeEach(inject([UserService, Router, ActivatedRoute, Md2Toast], (userService: UserService, router: Router,route: ActivatedRoute,toast: Md2Toast) => {
-//        userAddComponent = new UserAddComponent(userService, router, route, toast);
-//    }));
-//    it("should be defined", () => {
-//        expect(userAddComponent).toBeDefined();
-//    });
-//    it("should check user first name before add", inject([UserModel], (userModel: UserModel) => {
-//        let expectedFristName = "First Name";
-//        userModel.FirstName = expectedFristName;
-//        userAddComponent.addUser(userModel);
-//        expect(userModel.FirstName).toBe(expectedFristName);
-//    }));
-//    it("should check user email before add", () => {
-//        let email = "test@promactinfo.com";
-//        let result = true;
-//        let method = userAddComponent.checkEmail(email);
-//        expect(method).not.toBe(result);
-//    });
+    });
+    beforeEach(inject([UserService, Router, ActivatedRoute, Md2Toast], (userService: UserService, router: Router,route: ActivatedRoute,toast: Md2Toast) => {
+        userAddComponent = new UserAddComponent(userService, router, route, toast);
+    }));
+    it("should be defined", () => {
+        expect(userAddComponent).toBeDefined();
+    });
+    it("should check user first name before add", inject([UserModel], (userModel: UserModel) => {
+        let expectedFristName = "First Name";
+        userModel.FirstName = expectedFristName;
+        userAddComponent.addUser(userModel);
+        expect(userModel.FirstName).toBe(expectedFristName);
+    }));
+    it("should check user email before add", () => {
+        let email = "test@promactinfo.com";
+        let result = true;
+        let method = userAddComponent.checkEmail(email);
+        expect(method).not.toBe(result);
+    });
 
-//    it("get role test", () => {
-//        userAddComponent.getRoles();
-//        expect(userAddComponent.listOfRoles.length).toEqual(1);
-//    });
+    it("get role test", () => {
+        userAddComponent.getRoles();
+        expect(userAddComponent.listOfRoles.length).toEqual(1);
+    });
 
-//});    
+});    
--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user-list/user-list.component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user-list/user-list.component.spec.ts
@@ -1,46 +1,46 @@
-﻿//declare var describe, it, beforeEach, expect;
-//import {async, inject, TestBed, ComponentFixture} from '@angular/core/testing';
-//import {provide} from "@angular/core";
-//import { UserService }   from '../user.service';
-//import {UserModel} from '../user.model';
-//import { Component } from '@angular/core';
-//import { DeprecatedFormsModule } from '@angular/common';
-//import {ROUTER_DIRECTIVES, Router } from '@angular/router';
-//import {Md2Toast} from 'md2/toast';
-//import {UserListComponent} from '../user-list/user-list.component';
-//import {MockToast} from "../../shared/mocks/mock.toast";
-//import {TestConnection} from "../../shared/mocks/test.connection";
-//import {MockUserService} from "../../shared/mocks/user/mock.user.service";
-//import {MockBaseService} from '../../shared/mocks/mock.base';
+﻿declare var describe, it, beforeEach, expect;
+import {async, inject, TestBed, ComponentFixture} from '@angular/core/testing';
+import {provide} from "@angular/core";
+import { UserService }   from '../user.service';
+import {UserModel} from '../user.model';
+import { Component } from '@angular/core';
+import { DeprecatedFormsModule } from '@angular/common';
+import {ROUTER_DIRECTIVES, Router } from '@angular/router';
+import {Md2Toast} from 'md2/toast';
+import {UserListComponent} from '../user-list/user-list.component';
+import {MockToast} from "../../shared/mocks/mock.toast";
+import {TestConnection} from "../../shared/mocks/test.connection";
+import {MockUserService} from "../../shared/mocks/user/mock.user.service";
+import {MockBaseService} from '../../shared/mocks/mock.base';
 
-//describe("Project List Test", () => {
-//    let userListComponent: UserListComponent;
-//    class MockRouter { }
-//    beforeEach(() => {
-//        TestBed.configureTestingModule({
-//            providers: [
-//                provide(Router, { useClass: MockRouter }),
-//                provide(TestConnection, { useClass: TestConnection }),
-//                provide(UserService, { useClass: MockUserService }),
-//                provide(Md2Toast, { useClass: MockToast }),
-//                provide(MockBaseService, { useClass: MockBaseService})
-//            ]
-//        });
+describe("Project List Test", () => {
+    let userListComponent: UserListComponent;
+    class MockRouter { }
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                provide(Router, { useClass: MockRouter }),
+                provide(TestConnection, { useClass: TestConnection }),
+                provide(UserService, { useClass: MockUserService }),
+                provide(Md2Toast, { useClass: MockToast }),
+                provide(MockBaseService, { useClass: MockBaseService})
+            ]
+        });
 
-//    });
+    });
 
-//    beforeEach(inject([UserService, Router], (userService: UserService, router: Router) => {
-//        userListComponent = new UserListComponent(userService,router);
-//    }));
-//    it("should be defined", () => {
-//        expect(userListComponent).toBeDefined();
-//    });
-//     /**
-//     * get user of Projects
-//     */
-//    it("should get list of Project on initialization", () => {
-//        userListComponent.ngOnInit();
-//        expect(userListComponent.getUsers());
-//    });
+    beforeEach(inject([UserService, Router], (userService: UserService, router: Router) => {
+        userListComponent = new UserListComponent(userService,router);
+    }));
+    it("should be defined", () => {
+        expect(userListComponent).toBeDefined();
+    });
+     /**
+     * get user of Projects
+     */
+    it("should get list of Project on initialization", () => {
+        userListComponent.ngOnInit();
+        expect(userListComponent.getUsers());
+    });
 
-//});
+});
