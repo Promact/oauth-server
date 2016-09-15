d82c92dc7e7519dc6580c95e727b208e08536090
ankit <ankit@PR1C2R4Y2015>
2016-09-15 17:20:15 +0530

    update karma.config.js and package.json and .travis.yml

--- Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine', 'requirejs', 'es6-shim'],
+        frameworks: ['jasmine'],
 
         // list of files / patterns to load in the browser
         files: [// Polyfills.
@@ -115,7 +115,6 @@ module.exports = function (config) {
 
         // Karma plugins loaded
         plugins: [
-            'karma-es6-shim',
             'karma-jasmine',
             'karma-chrome-launcher'
         ],
