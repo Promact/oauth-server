ed08e73449a2ce7500bc81fb4775759049c41189
Ronak <ronak.shah@promactinfo.com>
2016-10-04 18:11:15 +0530

    Updated project-list-component.ts

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-list/project-list.component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-list/project-list.component.spec.ts
@@ -3,7 +3,7 @@
 //import { Provider, DebugElement } from "@angular/core";
 //import { ProjectService }   from '../project.service';
 //import {projectModel} from '../project.model';
-//import { Router } from '@angular/router';
+//import { Router, RouterModule, Routes} from '@angular/router';
 //import {ProjectListComponent} from './project-list.component';
 //import {TestConnection} from "../../shared/mocks/test.connection";
 //import {MockProjectService} from "../../shared/mocks/project/mock.project.service";
@@ -12,7 +12,7 @@
 //import { ProjectModule } from '../project.module';
 //import { Md2Toast } from 'md2/toast/toast';
 //import { LoaderService } from '../../shared/loader.service';
-//import { RouterLinkStubDirective } from '../../shared/mock.routerLink';
+//import { RouterLinkStubDirective } from '../../shared/mocks/mock.routerLink';
 
 //declare var describe, it, beforeEach, expect;
 //let comp: ProjectListComponent;
@@ -26,9 +26,13 @@
 //    class McokLogin { }
 //    class Md2Toast { }
 //    class MockLoaderService { }
+//    const routes: Routes = [];
 //    beforeEach(async(() => {
-//        this.promise =TestBed.configureTestingModule({
-//            imports: [ProjectModule],
+//        this.promise = TestBed.configureTestingModule({
+//            declarations: [RouterLinkStubDirective], //Declaration of mock routerLink used on page.
+//            imports: [ProjectModule
+//                , RouterModule.forRoot(routes, { useHash: true }) //Set LocationStrategy for component. 
+//            ],
 //            providers: [
 //                { provide: Router, useClass: MockRouter },
 //                { provide: ProjectService, useClass: MockProjectService },
@@ -37,14 +41,20 @@
 //                { provide: LoaderService, useClass: MockLoaderService }
 //            ]
 //        }).compileComponents(); 
+//        this.promise.then(() => {
+//            this.fixture = TestBed.createComponent(ProjectListComponent); //Create instance of component            
+//            this.projectListComponent = this.fixture.componentInstance;
+//        });
+          
 //    }));
 
  
 //    it("should get default Project for company", done => {
 //        this.promise.then(() => {
-//            fixture = TestBed.createComponent(ProjectListComponent);
-//            projectListComponent = fixture.componentInstance;
+//            //fixture = TestBed.createComponent(ProjectListComponent);
+//            //projectListComponent = fixture.componentInstance;
 //            expect(projectListComponent).toBeDefined();
+//            done();
 //            //fixture.detectChanges();
 //            //let listOfProject = projectListComponent.getProjects();
 //            //expect(projectListComponent.projects.length).toEqual(1);
