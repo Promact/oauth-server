2f3b166fc94cd6d95923fd51065a881d291b9424
Ronak <ronak.shah@promactinfo.com>
2016-10-05 14:12:02 +0530

    Updated test cases

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-add/project-add.component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-add/project-add.component.spec.ts
@@ -1,70 +1,76 @@
-﻿//declare var describe, it, beforeEach, expect;
-////import {Provider} from "@angular/core";
-////import {projectModel} from "../project.model";
-////import {ProjectAddComponent} from "../project-add/project-add.component";
-////import {ProjectService} from "../project.service";
-////import {UserModel} from '../../users/user.model';
-////import { Router} from '@angular/router';
-////import { ActivatedRoute} from '@angular/router';
-//////import {Md2Toast} from 'md2/toast';
-////import {MockToast} from "../../shared/mocks/mock.toast";
-//////import {Md2Multiselect } from 'md2/multiselect';
-////import {TestConnection} from "../../shared/mocks/test.connection";
-////import {MockProjectService} from "../../shared/mocks/project/mock.project.service";
-////import {MockBaseService} from '../../shared/mocks/mock.base';
-////import {MockRouter} from '../../shared/mocks/mock.router';
-////import {Observable} from 'rxjs/Observable';
-////declare var describe, it, beforeEach, expect;
-
-////describe('Project Add Test', () => {
-////    let projectAddComponent: ProjectAddComponent;
-////    class MockRouter { }
-////    class MockActivatedRoute extends ActivatedRoute {
-////        constructor() {
-////            super();
-////            this.params = Observable.of({ });
-////        }
-////    }
+﻿declare var describe, it, beforeEach, expect;
+import { async, inject, TestBed, ComponentFixture } from '@angular/core/testing';
+import {Provider} from "@angular/core";
+import {projectModel} from "../project.model";
+import {ProjectAddComponent} from "../project-add/project-add.component";
+import {ProjectService} from "../project.service";
+import {UserModel} from '../../users/user.model';
+import { Router, ActivatedRoute, RouterModule, Routes} from '@angular/router';
+import { Md2Toast } from 'md2/toast/toast';
+import {MockToast} from "../../shared/mocks/mock.toast";
+import {Md2Multiselect } from 'md2/multiselect';
+import {MockProjectService} from "../../shared/mocks/project/mock.project.service";
+import {MockRouter} from '../../shared/mocks/mock.router';
+import { Observable } from 'rxjs/Observable';
+import { RouterLinkStubDirective } from '../../shared/mocks/mock.routerLink';
+import { ProjectModule } from '../project.module';
+let promise: TestBed;
 
 
-////    beforeEach(() => {
-////        TestBed.configureTestingModule({
-////            providers: [
-////                { provide: ActivatedRoute, useClass: MockActivatedRoute },
-////                { provide: Router, useClass: MockRouter },
-////                { provide: TestConnection, useClass: TestConnection },
-////                { provide: ProjectService, useClass: ProjectService },
-////                //{ provide: Md2Toast, useClass: MockToast },
-////                { provide: MockBaseService, useClass: MockBaseService },
-////                { provide: UserModel, useClass: UserModel },
-////                { provide: projectModel, useClass: projectModel },
-////            ]
-////        });
+describe('Project Add Test', () => {
+    let projectAddComponent: ProjectAddComponent;
+    class MockRouter { }
+    const routes: Routes = [];
+    class MockActivatedRoute extends ActivatedRoute {
+        constructor() {
+            super();
+            this.params = Observable.of({ });
+        }
+    }
 
-////    });
 
+    beforeEach(async(() => {
+        this.promise = TestBed.configureTestingModule({
+            declarations: [RouterLinkStubDirective], //Declaration of mock routerLink used on page.
+            imports: [ProjectModule,RouterModule.forRoot(routes, { useHash: true }) //Set LocationStrategy for component. 
+            ],
+            providers: [
+                { provide: ActivatedRoute, useClass: MockActivatedRoute },
+                { provide: Router, useClass: MockRouter },
+                { provide: ProjectService, useClass: ProjectService },
+                { provide: UserModel, useClass: UserModel },
+                { provide: projectModel, useClass: projectModel },
+            ]
+        }).compileComponents();
+            this.promise.then(() => {
+                this.fixture = TestBed.createComponent(ProjectAddComponent); //Create instance of component            
+            this.projectListComponent = this.fixture.componentInstance;
+        });
+    }));
+
+    it("should get default Project for company", done => {
+        this.promise.then(() => {
+            expect(projectAddComponent).toBeDefined();
+            done();
+        })
+    });
    
-////    beforeEach(inject([ActivatedRoute, Router, /*Md2Toast,*/ ProjectService], (route: ActivatedRoute, router: Router,/* toast: Md2Toast,*/ projectService: ProjectService) => {
-////        projectAddComponent = new ProjectAddComponent(route, router, /*toast,*/ projectService);
-////    }));
-////    it("should be defined", () => {
-////        expect(projectAddComponent).toBeDefined();
-////    });
-////    it("should get default page for Project", () => {
-////        projectAddComponent.ngOnInit();
-////        expect(projectAddComponent.Userlist).not.toBeNull();
-////    });
+
+    //it("should get default page for Project", () => {
+    //    projectAddComponent.ngOnInit();
+    //    expect(projectAddComponent.Userlist).not.toBeNull();
+    //});
 
     
-////    it("should check project name before add", inject([projectModel], (projectModel: projectModel) => {
-////        let expectedProjectName = "Tests Projects";
-////        projectModel.Name = expectedProjectName;
-////        let expectedSlackChannelName = "Test Slack Name";
-////        projectModel.SlackChannelName = expectedSlackChannelName;
-////        projectAddComponent.addProject(projectModel);
-////        expect(projectModel.Name).toBe(expectedProjectName);
-////    }));
-////});    
+    //it("should check project name before add", inject([projectModel], (projectModel: projectModel) => {
+    //    let expectedProjectName = "Tests Projects";
+    //    projectModel.Name = expectedProjectName;
+    //    let expectedSlackChannelName = "Test Slack Name";
+    //    projectModel.SlackChannelName = expectedSlackChannelName;
+    //    projectAddComponent.addProject(projectModel);
+    //    expect(projectModel.Name).toBe(expectedProjectName);
+    //}));
+});    
 
 
 
--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-list/project-list.component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-list/project-list.component.spec.ts
@@ -1,13 +1,11 @@
 ﻿//import {async,inject,TestBed,ComponentFixture} from '@angular/core/testing';
 //import { By } from "@angular/platform-browser";
-//import { Provider, DebugElement } from "@angular/core";
+//import { Provider } from "@angular/core";
 //import { ProjectService }   from '../project.service';
 //import {projectModel} from '../project.model';
 //import { Router, RouterModule, Routes} from '@angular/router';
 //import {ProjectListComponent} from './project-list.component';
-//import {TestConnection} from "../../shared/mocks/test.connection";
 //import {MockProjectService} from "../../shared/mocks/project/mock.project.service";
-//import {MockBaseService} from '../../shared/mocks/mock.base';
 //import { LoginService } from '../../login.service';
 //import { ProjectModule } from '../project.module';
 //import { Md2Toast } from 'md2/toast/toast';
@@ -17,7 +15,7 @@
 //declare var describe, it, beforeEach, expect;
 //let comp: ProjectListComponent;
 //let fixture: ComponentFixture<ProjectListComponent>;
-//let el: DebugElement;
+
 //let promise: TestBed;
 
 //describe("Project List Test", () => {
@@ -37,7 +35,7 @@
 //                { provide: Router, useClass: MockRouter },
 //                { provide: ProjectService, useClass: MockProjectService },
 //                { provide: Md2Toast, useClass: Md2Toast },
-//                { provide: LoginService, useClass: McokLogin },
+//                { provide: LoginService, useClass: LoginService },
 //                { provide: LoaderService, useClass: MockLoaderService }
 //            ]
 //        }).compileComponents(); 
@@ -51,13 +49,8 @@
  
 //    it("should get default Project for company", done => {
 //        this.promise.then(() => {
-//            //fixture = TestBed.createComponent(ProjectListComponent);
-//            //projectListComponent = fixture.componentInstance;
 //            expect(projectListComponent).toBeDefined();
 //            done();
-//            //fixture.detectChanges();
-//            //let listOfProject = projectListComponent.getProjects();
-//            //expect(projectListComponent.projects.length).toEqual(1);
 //        });
 //    });
 //});
