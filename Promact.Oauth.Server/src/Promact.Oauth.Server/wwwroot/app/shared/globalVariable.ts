c4fcfc5249262f0c4e937a8b7b3b58ea1b4f9508
Ronak <ronak.shah@promactinfo.com>
2016-10-06 10:06:25 +0530

    added global variable

---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/shared/globalVariable.ts
@@ -0,0 +1,16 @@
+﻿import { Injectable } from "@angular/core";
+
+@Injectable()
+export class MyService {
+    private myValue;
+
+    constructor() { }
+
+    setValue(val) {
+        this.myValue = val;
+    }
+
+    getValue() {
+        return this.myValue;
+    }
+}
