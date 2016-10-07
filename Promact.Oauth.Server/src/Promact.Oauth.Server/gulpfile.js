b3807dcfd55b0eb2fd51d3b0a7cf7847858c606b
Ronak <ronak.shah@promactinfo.com>
2016-10-07 20:19:09 +0530

    Updated gulpfile and package.json

--- Promact.Oauth.Server/src/Promact.Oauth.Server/gulpfile.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/gulpfile.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var gulp = require("gulp"),
+    gutil = require('gulp-util'),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
@@ -26,32 +27,65 @@ paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 paths.systemConfig = paths.webroot + "systemjs.config.js";
 
+gulp.task('default', function () {
+    //return gutil.log('Gulp is running!')
+  
+});
+
 gulp.task("copytowwwroot", function () {
+
+     var launch = require('./Properties/launchSettings.json');
+
+    // Holds information about the hosting environment.
+  
+     gutil.log(launch.profiles['IIS Express'].environmentVariables.ASPNETCORE_ENVIRONMENT);
+     var result = launch.profiles['IIS Express'].environmentVariables.ASPNETCORE_ENVIRONMENT;
     gulp.src([
          'node_modules/zone.js/dist/zone.js',
          'node_modules/reflect-metadata/Reflect.js',
          'node_modules/systemjs/dist/system.src.js',
          'node_modules/core-js/client/shim.min.js'
     ]).pipe(gulp.dest('./wwwroot/lib/'));
-
-    gulp.src([
-      'node_modules/@angular/**/*.js', '!node_modules/@angular/**/*.js.map'
-    ]).pipe(gulp.dest('./wwwroot/lib/@angular'));
-
-
-    gulp.src([
-    'node_modules/@angular2-material/**/*.js', '!node_modules/@angular2-material/**/*.js.map'
-    ]).pipe(gulp.dest('./wwwroot/lib/@angular2-material'));
+    if (result == "Production") {
+        gutil.log("test1");
+        gulp.src([
+          'node_modules/@angular/**/*.js', '!node_modules/@angular/**/*.js.map'
+        ]).pipe(gulp.dest('./wwwroot/lib/@angular'));
 
 
-    gulp.src([
-      'node_modules/rxjs/**/*.js', '!node_modules/rxjs/**/*.js.map'
-    ]).pipe(gulp.dest('./wwwroot/lib/rxjs'));
+        gulp.src([
+        'node_modules/@angular2-material/**/*.js', '!node_modules/@angular2-material/**/*.js.map'
+        ]).pipe(gulp.dest('./wwwroot/lib/@angular2-material'));
 
-    gulp.src([
-        'node_modules/md2/**/*.js', '!node_modules/md2/**/*.js.map'
-    ]).pipe(gulp.dest('./wwwroot/lib/md2'));
 
+        gulp.src([
+          'node_modules/rxjs/**/*.js', '!node_modules/rxjs/**/*.js.map'
+        ]).pipe(gulp.dest('./wwwroot/lib/rxjs'));
+
+        gulp.src([
+            'node_modules/md2/**/*.js', '!node_modules/md2/**/*.js.map'
+        ]).pipe(gulp.dest('./wwwroot/lib/md2'));
+    }
+    else {
+        gutil.log("test2");
+        gulp.src([
+          'node_modules/@angular/**/*.js'
+        ]).pipe(gulp.dest('./wwwroot/lib/@angular'));
+
+
+        gulp.src([
+        'node_modules/@angular2-material/**/*.js'
+        ]).pipe(gulp.dest('./wwwroot/lib/@angular2-material'));
+
+
+        gulp.src([
+          'node_modules/rxjs/**/*.js'
+        ]).pipe(gulp.dest('./wwwroot/lib/rxjs'));
+
+        gulp.src([
+            'node_modules/md2/**/*.js'
+        ]).pipe(gulp.dest('./wwwroot/lib/md2'));
+    }
 });
 
 
