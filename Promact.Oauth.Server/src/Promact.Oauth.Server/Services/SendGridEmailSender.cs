5f140d266ebdae9753fc6b90c018078b0adc01f4
julie <julie@promactinfo.com>
2016-10-14 14:41:02 +0530

    Implemented class for SendGrid mail sending implementation

---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Services/SendGridEmailSender.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using Promact.Oauth.Server.Constants;
+using Promact.Oauth.Server.Models;
+using System.Net.Mail;
+
+namespace Promact.Oauth.Server.Services
+{
+    public class SendGridEmailSender : IEmailSender
+    {
+        private readonly IOptions<AppSettings> _appSettings;
+
+        public SendGridEmailSender(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        public void SendEmail(string email, string subject, string message)
+        {
+            var myMessage = new SendGrid.SendGridMessage();
+            myMessage.AddTo(email);
+            myMessage.From = new MailAddress(_appSettings.Value.From, StringConstant.PromactName);
+            myMessage.Subject = subject;
+            myMessage.Text = message;
+
+            var transportWeb = new SendGrid.Web(_appSettings.Value.SendGridApi);
+            transportWeb.DeliverAsync(myMessage);
+        }
+
+    }
+}
