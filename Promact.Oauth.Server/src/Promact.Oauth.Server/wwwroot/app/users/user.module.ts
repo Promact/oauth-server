613979378f100dc6adc75671a630f65185d6e023
ankit <ankit@PR1C2R4Y2015>
2016-10-18 14:32:38 +0530

    Update uesr-list.component.ts and user-list.html and user.module.ts , DataFilterPipe.ts , System.config.js and also gulpfile.js , package.json

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user.module.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user.module.ts
@@ -7,19 +7,24 @@ import { UserDetailsComponent } from "./user-details/user-details.component";
 import { UserService } from "./user.service";
 import { userRoute } from "./user.routes";
 import { SharedModule } from '../shared/shared.module';
+import { DataFilterPipe } from "../DataFilterPipe";
+import { DataTableModule } from "angular2-datatable";
 
 
 @NgModule({
     imports: [
         userRoute,
-        SharedModule
+        SharedModule,
+        DataTableModule
     ],
     declarations: [
         UserComponent,
         UserListComponent,
         UserDetailsComponent, 
         UserAddComponent,
-        UserEditComponent
+        UserEditComponent,
+        DataTableModule,
+        DataFilterPipe
     ],
     providers: [
         UserService
