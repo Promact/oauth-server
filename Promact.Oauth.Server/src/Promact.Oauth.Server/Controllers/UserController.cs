e1531b9ce122511e86706c322347bd1240bf8d1b
julie <julie@promactinfo.com>
2016-10-18 12:28:54 +0530

    Exception and loggers added

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/UserController.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/UserController.cs
@@ -183,10 +183,12 @@ namespace Promact.Oauth.Server.Controllers
             }
             catch (ArgumentNullException argEx)
             {
+                _logger.LogInformation("Add User unsuccessful " + argEx.Message + argEx.ToString());
                 throw argEx;
             }
             catch (Exception ex)
             {
+                _logger.LogInformation("Add User unsuccessful " + ex.Message + ex.ToString());
                 throw ex;
             }
         }
@@ -432,10 +434,12 @@ namespace Promact.Oauth.Server.Controllers
             }
             catch(ArgumentNullException argEx)
             {
+                _logger.LogInformation("Resend Mail unsuccessful "+argEx.Message+argEx.ToString());
                 throw argEx;
             }
             catch (Exception ex)
             {
+                _logger.LogInformation("Resend Mail unsuccessful " + ex.Message + ex.ToString());
                 throw ex;
             }
         }
