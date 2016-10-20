0608512701581a5dc704db8415842fb0cccee62b
Ronak <ronak.shah@promactinfo.com>
2016-10-20 14:16:43 +0530

    Added userrole model  and removed getRole method in user module and project list component

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.module.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginService } from "./login.service";
 import { MdSidenavModule } from "@angular2-material/sidenav";
 import { MdToolbarModule } from "@angular2-material/toolbar";
 import { LoaderService } from "./shared/loader.service";
+import { UserRole } from "./shared/userrole.model";
 
 import { DataTableModule } from "angular2-datatable";
 import { UserRole } from "./shared/userrole.model";
