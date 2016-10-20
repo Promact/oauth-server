46513e75c78dd05fbfda9c592dd7ae14e703e8ec
Ronak <ronak.shah@promactinfo.com>
2016-10-20 11:11:59 +0530

    remove globalvariable.ts

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.component.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/app.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { LoaderService } from './shared/loader.service'
 import { UserRole } from "./shared/userrole.model";
 
+
 @Component({
     selector: 'my-app',
     templateUrl: './app/index.html'
