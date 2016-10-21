cbd143c114540442f01aa2207dd9881627e2423f
julie <julie@promactinfo.com>
2016-10-21 16:54:26 +0530

    Code in IOAuthRepository.cs and OAuthRepository.cs changed according to coding conventions

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/OAuthRepository/IOAuthRepository.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/OAuthRepository/IOAuthRepository.cs
@@ -5,8 +5,29 @@ namespace Promact.Oauth.Server.Repository.OAuthRepository
 {
     public interface IOAuthRepository
     {
+        /// <summary>
+        /// Fetches the details of Client using access token
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <returns>true if value is not null otherwise false</returns>
         bool GetDetailsClientByAccessToken(string accessToken);
+
+
+        /// <summary>
+        /// Returns appropriate Url for the user to be redirected to
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="clientId"></param>
+        /// <param name="callBackUrl"></param>
+        /// <returns>returUrl</returns>
         Task<string> UserAlreadyLogin(string userName, string clientId, string callBackUrl);
+
+
+        /// <summary>
+        /// Signs in the user and redirects to the appropraite url.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>if not successfully signed-in then return empty string if successfully signed-in and credentials match then return redirect url</returns>
         Task<string> UserNotAlreadyLogin(OAuthLogin model);
     }
 }
