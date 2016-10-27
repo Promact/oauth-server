b93d1281e16dd59129ba213a6d97c80b59650368
Ronak <ronak.shah@promactinfo.com>
2016-10-27 14:40:01 +0530

    Updated ProjectConroller ,Projerct Service and Added projectNotFound.cs

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project.service.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project.service.ts
@@ -5,6 +5,7 @@ import { projectModel } from './project.model';
 @Injectable()
 export class ProjectService {
     private ProjectUrl = 'api/project';  // URL to web api
+    private UserUrl = 'api/user';
     constructor(private httpService: HttpService<projectModel>) { }
     //check duplicate
     checkDuplicate(project: projectModel) {
@@ -13,24 +14,19 @@ export class ProjectService {
 
     //list of users
     getUsers() {
-        return this.httpService.get("api/user" + "/getEmployees");
+        return this.httpService.get(this.UserUrl + "/getEmployees");
     }
-    //
     getProjects() {
-        return this.httpService.get(this.ProjectUrl + "/");
+        return this.httpService.get(this.ProjectUrl);
     }
     getProject(id: number) {
         return this.httpService.get(this.ProjectUrl+ "/" +id);
     }
     addProject(project: projectModel) {
-        return this.httpService.post(this.ProjectUrl + "/", project);
-    }
-
-    deleteProject(projectId: number) {
-        return this.httpService.delete(this.ProjectUrl + "/deleteProject/" + projectId);
+        return this.httpService.post(this.ProjectUrl, project);
     }
     editProject(project: projectModel)
     {
-        return this.httpService.put(this.ProjectUrl + "/", project);
+        return this.httpService.put(this.ProjectUrl, project);
     }
 }
