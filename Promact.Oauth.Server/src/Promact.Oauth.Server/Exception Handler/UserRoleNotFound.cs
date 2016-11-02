9d33be2de585259df06b1ea781f8462c1cd84450
Ronak <ronak.shah@promactinfo.com>
2016-11-02 13:46:50 +0530

    Updated ProjectController , DataRepository and Project Repository

---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Exception Handler/UserRoleNotFound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Oauth.Server.Exception_Handler
+{
+    public class UserRoleNotFound : Exception
+    {
+        /// <summary>
+        /// Initializes Exception For User Role Not Found
+        /// </summary>
+        public UserRoleNotFound() : base()
+        {
+
+        }
+    }
+}
