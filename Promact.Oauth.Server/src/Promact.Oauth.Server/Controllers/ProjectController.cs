09d31f4b2266d927ccb8f4ab81a6355a32a479bd
Ronak <ronak.shah@promactinfo.com>
2016-11-04 18:31:54 +0530

    Updated ProjectController.cs and ProjectRepository.cs

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectController.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/ProjectController.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Authorization;
 using Promact.Oauth.Server.Exception_Handler;
 using Microsoft.Extensions.Logging;
 using Promact.Oauth.Server.Exception_Handler;
+using Exceptionless;
 
 
 
