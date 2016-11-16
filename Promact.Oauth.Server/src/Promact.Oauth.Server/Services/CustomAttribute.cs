8a982b63b0d81a4c1288cbfdab8d9ff1b690352b
julie <julie@promactinfo.com>
2016-11-16 17:22:15 +0530

    converted OnActionExecution to OnActionExecutionAsync. Fixes #146

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Services/CustomAttribute.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Services/CustomAttribute.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Promact.Oauth.Server.Services
 {
+
     public class CustomAttribute : ActionFilterAttribute
     {
         private readonly IOAuthRepository _oAuthRepository;
