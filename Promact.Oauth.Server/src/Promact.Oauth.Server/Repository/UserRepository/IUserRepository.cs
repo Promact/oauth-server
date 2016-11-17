007f8d0d8da2742277a10afc946adc1cf7625f6c
Gourav Agarwal <gourav@promactinfo.com>
2016-11-17 15:28:21 +0530

    added async methods and modified apidoc

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/UserRepository/IUserRepository.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/UserRepository/IUserRepository.cs
@@ -113,7 +113,7 @@ namespace Promact.Oauth.Server.Repository
         /// </summary>
         /// <param name="userId"></param>
         /// <returns>details of user</returns>
-        UserAc UserDetailById(string userId);
+        Task<UserAc> UserDetailById(string userId);
 
         /// <summary>
         /// Method is used to get the details of user by using their username
@@ -148,7 +148,7 @@ namespace Promact.Oauth.Server.Repository
         /// </summary>
         /// <param name="teamLeaderId"></param>
         /// <returns>list of projects with users for a specific teamleader</returns>
-        List<UserAc> GetProjectUsersByTeamLeaderId(string teamLeaderId);
+        Task<List<UserAc>> GetProjectUsersByTeamLeaderId(string teamLeaderId);
 
     }
 }
