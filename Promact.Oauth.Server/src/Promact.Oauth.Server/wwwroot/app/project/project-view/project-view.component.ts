528be71d45579bde1dd262d78b9b4acf15fbac4c
Ronak <ronak.shah@promactinfo.com>
2016-12-12 16:39:18 +0530

    Added SlackUserId and Fixes #189

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-view/project-view.component.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/project/project-view/project-view.component.ts
@@ -55,6 +55,7 @@ export class ProjectViewComponent implements OnInit {
                                 this.project.applicationUsers[j].NumberOfSickLeave = this.project.listUsers[i].NumberOfSickLeave;
                                 this.project.applicationUsers[j].JoiningDate = this.project.listUsers[i].JoiningDate;
                                 this.project.applicationUsers[j].SlackUserName = this.project.listUsers[i].SlackUserName;
+                                this.project.applicationUsers[j].SlackUserId = this.project.listUsers[i].SlackUserId
                             }
                         }
                     }
