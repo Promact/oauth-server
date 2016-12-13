c7e7e7a0afdb8b7aab03a5995585252315bfd974
Ronak <ronak.shah@promactinfo.com>
2016-12-13 18:21:58 +0530

    Added string constant in testcases , test service , consumerApp module and remove unused code. Fixes #181

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/shared/stringconstant.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/shared/stringconstant.ts
@@ -5,6 +5,7 @@
 export class StringConstant {
     constructor() { }
     admin = 'Admin';
+    employee = 'Employee';
     projectUrl = 'api/project';
     userUrl = 'api/user';
     slash = '/';
@@ -19,4 +20,24 @@ export class StringConstant {
     userDetail = '/user/details/';
     userEdit ='/user/edit'
     emailExtension = '@promactinfo.com';
+    projectName = 'test project';
+    slackChannelName = 'Test Slack Name';
+    firstName = 'test';
+    testfirstName ="First Name"
+    lastName = 'shah';
+    email = 'test@abc.com';
+    teamLeaderId = '2';
+    consumerappname = 'slack';
+    description = 'slack description';
+    callbackUrl = 'www.google.com';
+    authSecret = 'dsdsdsdsdsdsd';
+    authId = 'ASASs5454545455';
+    consumerapp = '/consumerapp';
+    consumerappEdit = '/consumerapp/edit';
+    consumerappAdd = '/consumerapp/add';
+    empty = '';
+    newPassword = 'test123';
+    oldPassword = 'test';
+    confirmPassword='test1234'
+
 }
