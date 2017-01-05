7b12adfd0a82022497eeadc0021041d52e7fef64
Ronak <ronak.shah@promactinfo.com>
2017-01-05 12:42:13 +0530

    Remove string constant and added string literals. and Fixes #181

--- Promact.Oauth.Server/src/Promact.Oauth.Server/StringLliterals/StringLiterals.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/StringLliterals/StringLiterals.cs
@@ -5,38 +5,18 @@ using System.Threading.Tasks;
 
 namespace Promact.Oauth.Server.StringLliterals
 {
-    public class Projects
-    {
-        public string title { get; set; }
-        public string DateFormate { get; set; }
-        public string TeamLeaderNotAssign { get; set; }
-    }
-
-    public class User
-    {
-        public string title { get; set; }
-        public string UserDetialTemplateFolderPath { get; set; }
-        public string UserPassword { get; set; }
-    }
     public class ConsumerApp
     {
         public string ATOZaTOz0TO9 { get; set; }
         public string ATOZ0TO9 { get; set; }
     }
-    public class CommanStringConstant
-    {
-        public string title { get; set; }
-        public string RoleAdmin { get; set; }
-        public string RoleTeamLeader { get; set; }
-        public string RoleEmployee { get; set; }
-
+    public class Account {
+        public string EmailNotExists { get; set; }
+        public string SuccessfullySendMail { get; set; }
     }
-
     public class StringLiterals
     {
-        public Projects Projects { get; set; }
-        public User User { get; set; }
+        public Account Account { get; set; }
         public ConsumerApp ConsumerApp { get; set; }
-        public CommanStringConstant CommanStringConstant { get; set; }
     }
 }
