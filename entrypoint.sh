42499d368d5608b09e6211a0d4ca55c5d9ecc410
Chintan Shah <chintan.shah12@outlook.com>
2017-01-12 21:32:08 +0530

    removed CMD and updated Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,3 +16,4 @@ sed -i 's/NUMBER_OF_CASUAL_LEAVE/'"$CasualLeave"'/g' appsettings.production.json
 sed -i 's/PROMACT_ERP_URL/'"$PromactErpUrl"'/g' appsettings.production.json
 
 jq '.ConnectionStrings.DefaultConnection |= "'"$ConnectionString"'" ' appsettings.json 
+dotnet /out/Promact.Oauth.Server.dll
