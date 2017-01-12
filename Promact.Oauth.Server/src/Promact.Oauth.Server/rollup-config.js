df74d9e1fa65e57f59545e69add324ca578c791d
ankit <ankit@PR1C2R4Y2015>
2017-01-12 10:59:27 +0530

    update package.json and added rollup-config.js

---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/rollup-config.js
@@ -0,0 +1,17 @@
+﻿import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs';
+import uglify      from 'rollup-plugin-uglify'
+
+export default {
+entry: 'wwwroot/app/main.js',
+dest: 'wwwroot/dist/build.js', // output a single application bundle
+sourceMap: false,
+format: 'iife',
+plugins: [
+    nodeResolve({jsnext: true, module: true}),
+    commonjs({
+        include: 'node_modules/rxjs/**',
+    }),
+    uglify()
+]
+}
