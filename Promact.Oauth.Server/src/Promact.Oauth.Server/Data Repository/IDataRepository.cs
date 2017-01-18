0b19172546c40949245776d9de9ce1ed0227843a
Ronak <ronak.shah@promactinfo.com>
2017-01-18 16:38:31 +0530

    Rebase commit  Fixes #189

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Data Repository/IDataRepository.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Data Repository/IDataRepository.cs
@@ -88,12 +88,6 @@ namespace Promact.Oauth.Server.Data_Repository
         Task<T> FirstAsync(Expression<Func<T, bool>> predicate);
 
         /// <summary>
-        /// Method fetches the first record based on the supplied function.
-        /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
-        Task<T> FirstAsync(Expression<Func<T, bool>> predicate);
-        /// <summary>
         /// Fetches the Single entity based on the function.
         /// </summary>
         /// <param name="predicate"></param>
