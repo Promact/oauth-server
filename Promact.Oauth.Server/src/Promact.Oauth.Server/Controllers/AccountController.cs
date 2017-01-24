6f892a37b32ee9edb0d595aecddb427425652409
Ronak <ronak.shah@promactinfo.com>
2017-01-24 12:08:53 +0530

    rebase commit

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/AccountController.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Controllers/AccountController.cs
@@ -122,10 +122,6 @@ namespace Promact.Oauth.Server.Controllers
                 {
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
                     // Send an email with this link
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
-                    //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
-                    //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
                     return RedirectToLocal(returnUrl);
