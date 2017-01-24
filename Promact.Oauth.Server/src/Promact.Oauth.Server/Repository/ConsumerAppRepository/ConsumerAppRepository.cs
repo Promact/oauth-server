d5230833099eaea492b2f180c3e6893d26628079
Ronak <ronak.shah@promactinfo.com>
2017-01-24 11:35:25 +0530

    resloved rebase conflict and Fixes #144

--- Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/ConsumerAppRepository/ConsumerAppRepository.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/ConsumerAppRepository/ConsumerAppRepository.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Promact.Oauth.Server.Constants;
 using Promact.Oauth.Server.ExceptionHandler;
 using Promact.Oauth.Server.StringLiterals;
 using Microsoft.Extensions.Options;
@@ -39,25 +38,25 @@ namespace Promact.Oauth.Server.Repository.ConsumerAppRepository
         /// <summary>
         /// This method used for get apps detail by client id. 
         /// </summary>
-        /// <param name="clientId"></param>
-        /// <returns></returns>
+        /// <param name="clientId">passed client Id</param>
+        /// <returns>Consumer App object</returns>
         public async Task<ConsumerApps> GetAppDetailsAsync(string clientId)
         {
-            return await _appsDataRepository.FirstOrDefaultAsync(x => x.AuthId == clientId);
+            return await _appsDataRepository.FirstAsync(x => x.AuthId == clientId);
         }
 
         /// <summary>
-        /// This method used for added consumer app and return primary key. -An
+        /// This method used for added consumer app and return consumerApps Id. -An
         /// </summary>
-        /// <param name="aapsObject"></param>
-        /// <returns></returns>
+        /// <param name="consumerApps">consumerApp object</param>
+        /// <returns>consumerApp Id</returns>
         public async Task<int> AddConsumerAppsAsync(ConsumerAppsAc consumerApps)
         {
             if (await _appsDataRepository.FirstOrDefaultAsync(x => x.Name == consumerApps.Name) == null)
             {
                 var consumerAppObject = _mapperContext.Map<ConsumerAppsAc, ConsumerApps>(consumerApps);
-                consumerAppObject.AuthId = GetRandomNumber(true);
-                consumerAppObject.AuthSecret = GetRandomNumber(false);
+                consumerAppObject.AuthId = GetRandomString(true);
+                consumerAppObject.AuthSecret = GetRandomString(false);
                 consumerAppObject.CreatedDateTime = DateTime.Now;
                 _appsDataRepository.AddAsync(consumerAppObject);
                 await _appsDataRepository.SaveChangesAsync();
@@ -71,7 +70,7 @@ namespace Promact.Oauth.Server.Repository.ConsumerAppRepository
         /// <summary>
         /// This method used for get list of apps. -An
         /// </summary>
-        /// <returns></returns>
+        /// <returns>list of ConsumerApps</returns>
         public async Task<List<ConsumerApps>> GetListOfConsumerAppsAsync()
         {
            return await _appsDataRepository.GetAll().ToListAsync(); 
@@ -80,8 +79,8 @@ namespace Promact.Oauth.Server.Repository.ConsumerAppRepository
         /// <summary>
         /// This method used for get consumer app object by id. -An
         /// </summary>
-        /// <param name="id">app object primary key</param>
-        /// <returns></returns>
+        /// <param name="id">ConsumerApp Id</param>
+        /// <returns>ConsumerApp Object</returns>
         public async Task<ConsumerApps> GetConsumerAppByIdAsync(int id)
         {
             ConsumerApps consumerApps = await _appsDataRepository.FirstOrDefaultAsync(x => x.Id == id);
@@ -94,10 +93,10 @@ namespace Promact.Oauth.Server.Repository.ConsumerAppRepository
 
 
         /// <summary>
-        /// This method used for update consumer app and return primary key. -An
+        /// This method used for update consumer app and return consumerApp Id. -An
         /// </summary>
-        /// <param name="apps"></param>
-        /// <returns></returns>
+        /// <param name="consumerApps">passed consumerApp Object</param>
+        /// <returns>consumerApp Id</returns>
         public async Task<int> UpdateConsumerAppsAsync(ConsumerApps consumerApps)
         {
 
@@ -119,18 +118,18 @@ namespace Promact.Oauth.Server.Repository.ConsumerAppRepository
         /// </summary>
         /// <param name="isAuthId">isAuthId = true (get random number for auth id.) and 
         /// isAuthId = false (get random number for auth secreate)</param>
-        /// <returns></returns>
-        private string GetRandomNumber(bool isAuthId)
+        /// <returns>random string</returns>
+        private string GetRandomString(bool isAuthId)
         {
             var random = new Random();
             if (isAuthId)
             {
-                return new string(Enumerable.Repeat(_stringLiterals.ConsumerApp.ATOZ0TO9, 15)
+                return new string(Enumerable.Repeat(_stringLiterals.ConsumerApp.CapitalAlphaNumericString, 15)
                   .Select(s => s[random.Next(s.Length)]).ToArray());
             }
             else
             {
-                return new string(Enumerable.Repeat(_stringLiterals.ConsumerApp.ATOZaTOz0TO9, 30)
+                return new string(Enumerable.Repeat(_stringLiterals.ConsumerApp.AlphaNumericString, 30)
                   .Select(s => s[random.Next(s.Length)]).ToArray());
             }
 
--- Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/ConsumerAppRepository/IConsumerAppRepository.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Repository/ConsumerAppRepository/IConsumerAppRepository.cs
@@ -8,38 +8,38 @@ namespace Promact.Oauth.Server.Repository.ConsumerAppRepository
     public interface IConsumerAppRepository 
     {
         /// <summary>
-        /// This method used for added Consumer apps and return primary key. -An
+        /// This method used for added consumer app and return consumerApps Id. -An
         /// </summary>
-        /// <param name="aaps"></param>
-        /// <returns></returns>
+        /// <param name="consumerApps">consumerApp object</param>
+        /// <returns>consumerApp Id</returns>
         Task<int> AddConsumerAppsAsync(ConsumerAppsAc aaps);
 
 
         /// <summary>
-        /// This method used for get list of consumer apps. -An
+        /// This method used for get list of apps. -An
         /// </summary>
-        /// <returns></returns>
+        /// <returns>list of ConsumerApps</returns>
         Task<List<ConsumerApps>> GetListOfConsumerAppsAsync();
 
         /// <summary>
         /// This method used for get apps detail by client id. 
         /// </summary>
-        /// <param name="clientId"></param>
-        /// <returns></returns>
+        /// <param name="clientId">passed client Id</param>
+        /// <returns>Consumer App object</returns>
         Task<ConsumerApps> GetAppDetailsAsync(string clientId);
 
         /// <summary>
         /// This method used for get consumer app object by id. -An
         /// </summary>
-        /// <param name="id">pass apps object primarykey</param>
-        /// <returns></returns>
+        /// <param name="id">ConsumerApp Id</param>
+        /// <returns>ConsumerApp Object</returns>
         Task<ConsumerApps> GetConsumerAppByIdAsync(int id);
 
         /// <summary>
-        /// This method used for update consumer app and return primary key. -An
+        /// This method used for update consumer app and return consumerApp Id. -An
         /// </summary>
-        /// <param name="apps"></param>
-        /// <returns></returns>
+        /// <param name="consumerApps">passed consumerApp Object</param>
+        /// <returns>consumerApp Id</returns>
         Task<int> UpdateConsumerAppsAsync(ConsumerApps consumerApps);
     }
 }
