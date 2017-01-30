a90b570b67420f0b22faeb032a55f4ee981324bd
Ronak <ronak.shah@promactinfo.com>
2017-01-30 11:23:48 +0530

    commit to rebuild. Fixes #144

--- Promact.Oauth.Server/src/Promact.Oauth.Server/StringLiterals/StringLiteral.cs
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/StringLiterals/StringLiteral.cs
@@ -1,6 +1,6 @@
 ﻿namespace Promact.Oauth.Server.StringLiterals
 {
-     public class StringLiteral
+    public class StringLiteral
     {
         public Account Account { get; set; }
         public ConsumerApp ConsumerApp { get; set; }
