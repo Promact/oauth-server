739c27383f62c22071c3ea37f5dcef74fd67ed39
ankit <ankit@PR1C2R4Y2015>
2017-02-20 12:09:02 +0530

    updated consumerapp-routes.ts and project.routes.ts ,user.routes.ts also added authentication.service.ts #264

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user.routes.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user.routes.ts
@@ -5,15 +5,16 @@ import {UserListComponent} from './user-list/user-list.component';
 import {UserAddComponent} from './user-add/user-add.component';
 import {UserEditComponent} from './user-edit/user-edit.component';
 import {UserDetailsComponent} from './user-details/user-details.component';
+import { authenticationService } from "../authentication.service";
 
 const userRoutes: Routes = [{
     path: "user",
     component: UserComponent,
     children: [
-        { path: '', component: UserListComponent },
-        { path: 'list', component: UserListComponent },
-        { path: 'add', component: UserAddComponent },
-        { path: 'edit/:id', component: UserEditComponent },
+        { path: '', component: UserListComponent, canActivate: [authenticationService] },
+        { path: 'list', component: UserListComponent, canActivate: [authenticationService]},
+        { path: 'add', component: UserAddComponent, canActivate: [authenticationService]},
+        { path: 'edit/:id', component: UserEditComponent, canActivate: [authenticationService] },
         { path: 'details/:id', component: UserDetailsComponent }
     ]
 }];
