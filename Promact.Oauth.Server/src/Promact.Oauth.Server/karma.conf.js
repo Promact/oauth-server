92ff170824877b52604e647b6463fa856102ca0b
Siddhartha <Siddhartha@PR2C5R2Y2015>
2017-02-28 15:29:03 +0530

    karma.conf.js -  added client side codecov for only component.js

--- Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/karma.conf.js
@@ -74,7 +74,8 @@ module.exports = function (config) {
         //// preprocess matching files before serving them to the browser
         //// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'wwwroot/app/**/!(*spec).js': ['coverage']
+            'wwwroot/app/*/*component.js': ['coverage'],
+            'wwwroot/app/**/*component.js': ['coverage']
         },
 
 
