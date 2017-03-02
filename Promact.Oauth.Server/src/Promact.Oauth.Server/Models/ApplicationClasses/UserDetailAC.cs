a2dde9ebfbd06cfa2a3b9021951a754385d9f948
ankit <ankit@PR1C2R4Y2015>
2017-03-02 18:03:22 +0530

    added UserDetailAc class and update project repository

---
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/Models/ApplicationClasses/UserDetailAC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Oauth.Server.Models.ApplicationClasses
+{
+    public class UserDetailAc
+    {
+        public List<string> TeamLeader { get; set; }
+
+        public List<string> TamMemeber { get; set; }
+
+        public List<string> Management { get; set; }
+    }
+}
