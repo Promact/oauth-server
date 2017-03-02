8a170eb3644b1be320a7aaa68e6c16f50843da6e
julie <julie@promactinfo.com>
2017-03-02 08:35:00 +0530

    added test cases for user-list.compponent. Fixes#277

--- Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user-list/user-list.component.spec.ts
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/wwwroot/app/users/user-list/user-list.component.spec.ts
@@ -1,5 +1,6 @@
-﻿declare var describe, it, beforeEach, expect;
+﻿declare var describe, it, beforeEach, expect, spyOn;
 import { async, TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { UserService } from '../user.service';
 import { UserModel } from '../user.model';
 import { RouterModule, Routes } from '@angular/router';
@@ -9,8 +10,8 @@ import { MockToast } from "../../shared/mocks/mock.toast";
 import { MockUserService } from "../../shared/mocks/user/mock.user.service";
 import { UserModule } from '../user.module';
 import { LoaderService } from '../../shared/loader.service';
-
-
+import { Router } from '@angular/router';
+let loader = new LoaderService();
 let fixture: ComponentFixture<UserListComponent>;
 
 describe("User List Test", () => {
@@ -24,10 +25,10 @@ describe("User List Test", () => {
                 { provide: UserService, useClass: MockUserService },
                 { provide: Md2Toast, useClass: MockToast },
                 { provide: LoaderService, useClass: MockLoaderService }
-
             ]
         }).compileComponents();
     }));
+
 
     it("should be defined userListComponent", () => {
         let fixture = TestBed.createComponent(UserListComponent);
@@ -39,10 +40,39 @@ describe("User List Test", () => {
     it("should get default Users for company", fakeAsync(() => {
         let fixture = TestBed.createComponent(UserListComponent); //Create instance of component            
         let userListComponent = fixture.componentInstance;
-        userListComponent.getUsers();
+        userListComponent.ngOnInit();
         tick();
         expect(userListComponent.users.length).not.toBeNull();
     }));
+
+
+    it('Calls userDetails', fakeAsync(() => {
+        let fixture = TestBed.createComponent(UserListComponent); //Create instance of component            
+        let userListComponent = fixture.componentInstance;
+        let userListService = fixture.debugElement.injector.get(UserService);
+        let router = fixture.debugElement.injector.get(Router);
+        spyOn(router, "navigate");
+        let userModel = new UserModel();
+        userModel.Id = "1";
+        userListComponent.userDetails(userModel.Id);
+        tick();
+        expect(router.navigate).toHaveBeenCalled();
+    }));
+
+
+    it('Calls userEdit', fakeAsync(() => {
+        let fixture = TestBed.createComponent(UserListComponent); //Create instance of component            
+        let userListComponent = fixture.componentInstance;
+        let userListService = fixture.debugElement.injector.get(UserService);
+        let router = fixture.debugElement.injector.get(Router);
+        spyOn(router, "navigate");
+        let userModel = new UserModel();
+        userModel.Id = "1";
+        userListComponent.userEdit(userModel.Id);
+        tick();
+        expect(router.navigate).toHaveBeenCalled();
+    }));
+
 
     it("should delete user", fakeAsync(() => {
         let fixture = TestBed.createComponent(UserListComponent); //Create instance of component            
@@ -51,8 +81,31 @@ describe("User List Test", () => {
         userModel.Id = "1";
         userListComponent.userDelete(userModel.Id);
         tick();
-        expect(userListComponent.users.length).not.toBeNull();
+        expect(userListComponent.users.length).not.toBe(0);
     }));
+
+
+    it("should not delete user", fakeAsync(() => {
+        let fixture = TestBed.createComponent(UserListComponent); //Create instance of component            
+        let userListComponent = fixture.componentInstance;
+        let userModel = new UserModel();
+        userModel.Id = "2";
+        userListComponent.userDelete(userModel.Id);
+        tick();
+        expect(userListComponent.users.length).toBe(0);
+    }));
+
+
+    it("should resend mail to user ", fakeAsync(() => {
+        let fixture = TestBed.createComponent(UserListComponent); //Create instance of component            
+        let userListComponent = fixture.componentInstance;
+        let userModel = new UserModel();
+        userModel.Id = "1";
+        userListComponent.reSendMail(userModel);
+        tick();
+        expect(userListComponent.reSendMail).toBeDefined();
+    }));
+
 });
 
 
