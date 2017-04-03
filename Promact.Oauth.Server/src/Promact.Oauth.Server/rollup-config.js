376bbeb8e6893a7288bb8ba4e6a717ab22ad463f
Siddhartha <Siddhartha@PR2C5R2Y2015>
2017-04-03 17:04:33 +0530

    rollup-config.js - added semicollon

--- Promact.Oauth.Server/src/Promact.Oauth.Server/rollup-config.js
+++ Promact.Oauth.Server/src/Promact.Oauth.Server/rollup-config.js
@@ -1,7 +1,7 @@
-﻿import rollup      from 'rollup'
-import nodeResolve from 'rollup-plugin-node-resolve'
+﻿import rollup      from 'rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
-import uglify      from 'rollup-plugin-uglify'
+import uglify      from 'rollup-plugin-uglify';
 export default {
   entry: 'wwwroot/app/main.js',
   dest: 'wwwroot/app/build.js', // output a single application bundle
